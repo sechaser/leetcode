9691fc906a77906bde1f2d03ccb770c732db1d04
sechaser <413406236@qq.com>
2016-10-05 20:23:33 +0800

    The 1 question

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
-
-using namespace std;
+#include <vector>
+#include <utility>
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::vector<int> iv = {2, 7, 11, 15};
+    int target = 26;
+    std::pair<size_t, size_t> p;
+
+    for(std::vector<int>::size_type i = 0; i != iv.size(); ++ i)
+    {
+        for(std::vector<int>::size_type j = i + 1; j != iv.size(); ++ j)
+        {
+            if(iv[i] + iv[j] == target)
+            {
+                p.first  = i;
+                p.second = j;
+            }
+        }
+    }
+    std::cout<<p.first<<" "<<p.second<<std::endl;
+
     return 0;
 }
 
