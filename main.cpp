d14200e6d9158dbd08d23682f2a5fa3306a3492f
sechaser <413406236@qq.com>
2016-10-07 20:35:47 +0800

    Question 3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
+#include <math.h>
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::string s = "aabbca";
+    int maxLen = 0, startIndex = 0;
+
+    if((s.size() == 0) || (s.size() == 1) )
+        return s.size();
+
+    for(std::string::size_type i = 1; i != s.size(); ++ i)
+    {
+        std::string::size_type j;
+        for(j = startIndex; j != i; ++ j)
+        {
+            if(s[j] != s[i])
+                continue;
+            else
+            {
+                if(i - startIndex > maxLen)
+                    maxLen = i - startIndex;
+                startIndex = j + 1;
+                break;
+            }
+        }
+        if(j == i)
+        {
+            if(i - startIndex >= maxLen)
+                maxLen = i - startIndex + 1;
+        }
+    }
+    std::cout<<"maxlen = "<<maxLen<<std::endl;
+
+    system("pause");
     return 0;
 }
 
