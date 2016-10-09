ed989748aa050c66191e11e1d7170a8ae68d57df
sechaser <413406236@qq.com>
2016-10-09 19:52:14 +0800

    Question 7

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <algorithm>
 
-using namespace std;
+#include <math.h>
+
+int reverse(int x)
+{
+    int res = 0;
+    while(x != 0)
+    {
+        if(res > INT_MAX / 10 || res < INT_MIN / 10)
+            return 0;
+        res = res * 10 + x % 10;
+        x = x / 10;
+    }
+
+    return res;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int x = 1111111115;
+    int res = reverse(x);
+    std::cout<<res<<std::endl;
+    std::cout<<INT_MAX<<" "<<INT_MIN<<std::endl;
+
+    std::cout<<pow(2, 31)<<std::endl;
+
     return 0;
 }
 
