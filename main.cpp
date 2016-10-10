d9b7fcbad6d5efcba590f7ca28b73f9ac0ddacf6
sechaser <413406236@qq.com>
2016-10-10 17:48:12 +0800

    Question 10

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <regex>
 
-using namespace std;
+bool isMatch(std::string s, std::string p)
+{
+    std::regex pattern(p);
+
+    if(std::regex_match(s, pattern))
+        return true;
+    else
+        return false;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::string p = "c*a*b";
+    std::string s = "aab";
+    bool res;
+    res = isMatch(s, p);
+    std::cout<<res<<std::endl;
+
     return 0;
 }
 
