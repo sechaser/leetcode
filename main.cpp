a96cee15981b1dca8d6b6b40768eb8dacdf195ce
sechaser <413406236@qq.com>
2016-10-10 13:04:33 +0800

    Question 9

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
+bool isPalindrome(int x)
+{
+    std::string str;
+    std::stringstream ss;
+    while(x != 0)
+    {
+        ss<<(x % 10);
+        x = x / 10;
+    }
+
+    for(int i = ss.str().size() - 1; i >= 0; -- i)
+        str += ss.str()[i];
+
+    std::string::size_type sz = str.size();
+    for(std::string::size_type i = 0; i != sz/2; ++ i)
+    {
+        if(str[i] == str[sz-i-1])
+            continue;
+        else
+            return false;
+    }
+    return true;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::stringstream ss;
+    int x = 121;
+    bool res = isPalindrome(x);
+
+    std::cout<<res<<std::endl;
+    system("pause");
     return 0;
 }
 
