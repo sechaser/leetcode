c1c737b387264d15e878234a56d5b6b4be7d4acd
sechaser <413406236@qq.com>
2016-10-10 12:34:03 +0800

    Question 8

--- main.cpp
+++ main.cpp
@@ -1,10 +1,73 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
 
-using namespace std;
+int myAtoi(std::string str)
+{
+    if(str.size() == 0)
+        return 0;
+
+    double res = 0;
+    bool signal = 0, positive = 1;
+    std::string s;
+
+    std::string::size_type j = 0;
+    while(str[j] == ' ')
+        ++ j;
+
+    for(std::string::size_type i = j; i != str.size(); ++ i)
+    {
+        if(str[i] == '+')
+        {
+            if(signal)
+                break;
+            else
+                signal = 1;
+        }
+        else if(str[i] == '-')
+        {
+            if(signal)
+                break;
+            else
+            {
+                positive = 0;
+                signal = 1;
+            }
+        }
+        else if(!isdigit(str[i]))
+            break;
+        else
+        {
+            s = str.substr(i, 1);
+            res = 10 * res + atoi(s.c_str());
+        }
+    }
+
+    if(positive)
+    {
+        if(res > INT_MAX)
+            return INT_MAX;
+        else
+            return res;
+    }
+    else
+    {
+        if((-res) < INT_MIN)
+            return INT_MIN;
+        else
+            return (-res);
+    }
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::string str = "922337203684775809";
+    int res = myAtoi(str);
+
+    std::cout<<res<<std::endl;
+    std::cout<<INT_MAX<<" "<<INT_MIN<<std::endl;
+
+    system("pause");
     return 0;
 }
 
