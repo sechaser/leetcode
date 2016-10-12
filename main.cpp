5a5eae7f282df9a341f2362bec784d70481710ff
sechaser <413406236@qq.com>
2016-10-12 19:21:50 +0800

    Question 14

--- main.cpp
+++ main.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-using namespace std;
+std::string longestCommonPrefix(std::vector<std::string>& strs)
+{
+    std::string prefix = "";
+    if(strs.empty())
+        return prefix;
+    else
+    {
+        std::string::size_type minLen = strs[0].size();
+        for(std::vector<std::string>::size_type t = 0; t != strs.size(); ++ t)
+        {
+            if(strs[t].size() < minLen)
+                minLen = strs[t].size();
+        }
+
+        char ch;
+        for(std::string::size_type len = 0; len != minLen; ++ len)
+        {
+            ch = strs[0][len];
+            for(std::vector<std::string>::size_type t = 1; t != strs.size(); ++ t)
+            {
+                if(strs[t][len] != ch)
+                    return prefix;
+                else
+                    continue;
+            }
+            prefix += ch;
+        }
+    }
+    return prefix;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::vector<std::string> strs = {"hello", "h1l", "hellllo"};
+    std::string res = longestCommonPrefix(strs);
+    std::cout<<res<<std::endl;
+
     return 0;
 }
 
