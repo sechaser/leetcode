007f7caad547e2bb0f908304433f8e7f11cab090
sechaser <413406236@qq.com>
2016-10-12 18:39:32 +0800

    Question 11

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-using namespace std;
+int maxArea(std::vector<int>& height)
+{
+    int area = 0;
+    int maxArea = 0;
+    std::vector<int>::size_type i = 0, j = height.size() - 1;
+
+    while(i < j)
+    {
+        area = (j - i) * std::min(height[i], height[j]);
+
+        if(height[i] < height[j])
+            ++ i;
+        else
+            -- j;
+
+        if(area > maxArea)
+            maxArea = area;
+    }
+
+    return maxArea;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
     return 0;
 }
 
