c833254373c9d290ecf6ad36eaf52bfdf345ff7c
sechaser <413406236@qq.com>
2016-10-16 08:36:06 +0800

    4Sum add second solution

--- main.cpp
+++ main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
+
+//std::vector<std::vector<int> > fourSum(std::vector<int>& nums, int target)
+//{
+//    size_t sz = nums.size();
+//    std::sort(nums.begin(), nums.end());
+//    std::vector<std::vector<int> > res;
+
+//    if(sz < 4)
+//        return res;
+
+//    int target2;
+//    int head, tail, temp;
+//    for(std::vector<int>::size_type first = 0; first != sz-3; ++ first)
+//    {
+//        if(first > 0 && nums[first] == nums[first-1])
+//            continue;
+
+//        for(std::vector<int>::size_type second = first+1; second != sz-2; ++ second)
+//        {
+//            if(second > first+1 && nums[second] == nums[second-1])
+//                continue;
+
+//            target2 = target - (nums[first] + nums[second]);
+//            head = second + 1;
+//            tail = sz - 1;
+
+//            while(head < tail)
+//            {
+//                temp = nums[head] + nums[tail];
+//                if(temp < target2)
+//                    ++ head;
+//                else if(temp > target2)
+//                    -- tail;
+//                else
+//                {
+//                    std::vector<int> oneres{nums[first], nums[second], nums[head], nums[tail]};
+//                    res.push_back(oneres);
+
+//                    int k = head + 1;
+//                    while(k < tail && nums[k] == nums[head])
+//                        ++ k;
+//                    head = k;
+
+//                    k = tail - 1;
+//                    while(head < k && nums[k] == nums[tail])
+//                        -- k;
+//                    tail = k ;
+//                }
+//            }
+//        }
+//    }
+
+//    return res;
+//}
 
 std::vector<std::vector<int> > fourSum(std::vector<int>& nums, int target)
 {
@@ -11,43 +66,40 @@ std::vector<std::vector<int> > fourSum(std::vector<int>& nums, int target)
     if(sz < 4)
         return res;
 
-    int target2;
-    int head, tail, temp;
-    for(std::vector<int>::size_type first = 0; first != sz-3; ++ first)
-    {
-        if(first > 0 && nums[first] == nums[first-1])
-            continue;
+    std::unordered_map<int, std::vector<std::pair<int, int> >  > pairs;
+    pairs.reserve(sz * sz);
+    std::sort(nums.begin(), nums.end());
 
-        for(std::vector<int>::size_type second = first+1; second != sz-2; ++ second)
+    for(std::vector<int>::size_type i = 0; i != sz; ++ i)
+    {
+        for(std::vector<int>::size_type j = i+1; j != sz; ++ j)
+            pairs[nums[i]+nums[j]].push_back(std::make_pair(i,j));
+    }
+
+    for(std::vector<int>::size_type i = 0; i != sz-3; ++ i)
+    {
+        if(i > 0 && nums[i] == nums[i-1])
+            continue;
+        for(std::vector<int>::size_type j = i+1; j != sz-2; ++ j)
         {
-            if(second > first+1 && nums[second] == nums[second-1])
+            if(j > i+1 && nums[j] == nums[j-1])
                 continue;
 
-            target2 = target - (nums[first] + nums[second]);
-            head = second + 1;
-            tail = sz - 1;
-
-            while(head < tail)
+            if(pairs.find(target-nums[i]-nums[j]) != pairs.end())
             {
-                temp = nums[head] + nums[tail];
-                if(temp < target2)
-                    ++ head;
-                else if(temp > target2)
-                    -- tail;
-                else
+                std::vector<std::pair<int, int> > twoSum = pairs[target-nums[i]-nums[j]];
+                bool numberone = 1;
+
+                for(std::vector<std::pair<int, int> >::size_type k = 0; k != twoSum.size(); ++ k)
                 {
-                    std::vector<int> oneres{nums[first], nums[second], nums[head], nums[tail]};
-                    res.push_back(oneres);
+                    if(twoSum[k].first <= j)
+                        continue;
 
-                    int k = head + 1;
-                    while(k < tail && nums[k] == nums[head])
-                        ++ k;
-                    head = k;
-
-                    k = tail - 1;
-                    while(head < k && nums[k] == nums[tail])
-                        -- k;
-                    tail = k ;
+                    if(numberone || (res.back()[2] != nums[twoSum[k].first]))
+                    {
+                        res.push_back(std::vector<int>{nums[i], nums[j], nums[twoSum[k].first], nums[twoSum[k].second]});
+                        numberone = 0;
+                    }
                 }
             }
         }
