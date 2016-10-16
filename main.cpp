0924c7b4b9fd9cf7aa375fea04f786cce9e920c3
sechaser <413406236@qq.com>
2016-10-16 14:25:57 +0800

    Valid Parentheses

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <string>
+#include <stack>
 
-using namespace std;
+bool isValid(std::string s)
+{
+    std::stack<char> stac;
+    std::string::size_type sz = s.size();
+
+    for(std::string::size_type i = 0; i != sz; ++ i)
+    {
+        if(stac.empty())
+            stac.push(s[i]);
+        else if((stac.top() == '(' && s[i]== ')') || (stac.top() == '[' && s[i]== ']') || (stac.top() == '{' && s[i]== '}'))
+            stac.pop();
+        else
+            stac.push(s[i]);
+    }
+
+    if(stac.empty())
+        return true;
+    else
+        return false;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::string s = "[()]";
+    bool res = isValid(s);
+    std::cout<<res<<std::endl;
+
     return 0;
 }
 
