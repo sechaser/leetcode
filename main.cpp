de9171fc9ccc3f881c68ff51afde796455730fc1
sechaser <413406236@qq.com>
2016-10-17 20:13:23 +0800

    Update master branch

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 
 struct ListNode
 {
@@ -11,8 +12,65 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+
 int main()
 {
+    ListNode* node, *p, *lis1, *lis2, *lis3;
+    for(int i = 0; i != 5; ++ i)
+    {
+        node = (ListNode*)malloc(sizeof(ListNode));
+        node->val  = i;
+        node->next = NULL;
+
+        if(i == 0)
+        {
+            lis1 = node;
+            p    = node;
+        }
+        else
+        {
+            p->next = node;
+            p       = node;
+        }
+    }
+
+
+    for(int i = 0; i != 5; ++ i)
+    {
+        node = (ListNode*)malloc(sizeof(ListNode));
+        node->val  = i+5;
+        node->next = NULL;
+
+        if(i == 0)
+        {
+            lis2 = node;
+            p    = node;
+        }
+        else
+        {
+            p->next = node;
+            p       = node;
+        }
+    }
+
+    for(int i = 0; i != 5; ++ i)
+    {
+        node = (ListNode*)malloc(sizeof(ListNode));
+        node->val  = i;
+        node->next = NULL;
+
+        if(i == 0)
+        {
+            lis3 = node;
+            p    = node;
+        }
+        else
+        {
+            p->next = node;
+            p       = node;
+        }
+    }
+
 
     system("pause");
     return 0;
