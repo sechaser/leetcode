97d91f73e9a6afe9fde5a42533b8fd59336c6d4c
sechaser <413406236@qq.com>
2016-10-18 20:41:29 +0800

    25 Reverse Nodes in k-Group

--- main.cpp
+++ main.cpp
@@ -12,10 +12,58 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+ListNode* reverse(ListNode* pre, ListNode* end)
+{
+    if(pre == NULL || pre->next == NULL)
+        return pre;
+
+    ListNode* head = pre->next;
+    ListNode* cur  = pre->next->next;
+
+    while(cur != end)
+    {
+        ListNode* next = cur->next;
+        cur->next = pre->next;
+        pre->next = cur;
+        cur = next;
+    }
+
+    head->next = end;
+    return head;
+}
+
+ListNode* reverseKGroup(ListNode* head, int k)
+{
+    if(head == NULL)
+        return NULL;
+
+    ListNode* node = new ListNode(0);
+    node->next = head;
+    int count = 0;
+
+    ListNode* pre = node;
+    ListNode* cur = head;
+
+    while(cur != NULL)
+    {
+        ++ count;
+        ListNode* next = cur->next;
+
+        if(count == k)
+        {
+            pre = reverse(pre, next);
+            count = 0;
+        }
+
+        cur = next;
+    }
+
+    return node->next;
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
+    ListNode* node, *p, *lis1;
     for(int i = 0; i != 5; ++ i)
     {
         node = (ListNode*)malloc(sizeof(ListNode));
@@ -34,43 +82,13 @@ int main()
         }
     }
 
-
-    for(int i = 0; i != 5; ++ i)
+    ListNode* newList = reverseKGroup(lis1, 2);
+    while(newList != NULL)
     {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
+        std::cout<<std::setw(4)<<newList->val;
+        newList = newList->next;
     }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::cout<<std::endl;
 
     system("pause");
     return 0;
