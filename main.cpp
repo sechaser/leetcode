f7d18e4dd061c7272b6ee0988e1a9a0d174a43fb
sechaser <413406236@qq.com>
2016-10-23 20:38:21 +0800

    31.Next Permutation

--- main.cpp
+++ main.cpp
@@ -5,72 +5,51 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+void nextPermutation(std::vector<int>& nums)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    std::vector<int>::size_type nums_sz = nums.size();
+
+    if(nums_sz > 1)
+    {
+        int i;
+        bool flag = 0;
+        for(i = nums_sz - 1; i >= 1; -- i)
+        {
+            if(nums[i] <= nums[i - 1])
+                continue;
+            else
+            {
+                int minIndex = i;
+                int j;
+                for(j = i + 1; j != nums_sz; ++ j)
+                {
+                    if(nums[j] < nums[minIndex] && nums[j] > nums[i - 1])
+                        minIndex = j;
+                }
+
+                int temp = nums[i - 1];
+                nums[i - 1] = nums[minIndex];
+                nums[minIndex] = temp;
+                std::sort(nums.begin() + i, nums.end());
+
+                flag = 1;
+                break;
+            }
+        }
+
+        if(!flag)
+            std::sort(nums.begin(), nums.end());
+    }
+}
 
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
+    std::vector<int> nums{3, 2, 1};
+    nextPermutation(nums);
 
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    for(std::vector<int>::size_type i = 0; i != nums.size(); ++ i)
+        std::cout<<nums[i]<<std::endl;
 
     system("pause");
     return 0;
