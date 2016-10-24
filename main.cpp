16d8e579a50933f260766f6feb44d8db7ccbb0b0
sechaser <413406236@qq.com>
2016-10-24 19:10:24 +0800

    32. Longest valid parentheses

--- main.cpp
+++ main.cpp
@@ -4,73 +4,61 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <stack>
+#include <utility>
 
-struct ListNode
+int longestValidParentheses(std::string s)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    if(s.empty())
+        return 0;
+    else
+    {
+        int maxlen = 0;
+        int templen = 0;
+        std::stack<int> indexSt;
 
+        for(std::string::size_type i = 0; i != s.size(); ++ i)
+        {
+            if(s[i] == '(')
+                indexSt.push(i);
+            else
+            {
+                if(indexSt.empty())
+                {
+                    std::cout<<"1.i = "<<i<<std::endl;
+                    indexSt.push(i);
+                }
+                else if(!indexSt.empty() && s[indexSt.top()] != '(')
+                {
+                    std::cout<<"2.i = "<<i<<std::endl;
+                    indexSt.push(i);
+                }
+                else
+                {
+                    std::cout<<"3.i = "<<i<<std::endl;
+                    indexSt.pop();
+                    if(indexSt.empty())
+                        templen = i + 1;
+                    else
+                        templen = i - indexSt.top();
+
+                    if(templen > maxlen)
+                        maxlen = templen;
+                }
+            }
+
+            std::cout<<"i = "<<i<<" temp = "<<templen<<" max = "<<maxlen<<std::endl;
+        }
+
+        return maxlen;
+    }
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::string s = "())()()";
+    int res = longestValidParentheses(s);
+    std::cout<<res<<std::endl;
 
     system("pause");
     return 0;
