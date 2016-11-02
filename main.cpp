e311b417f937be9a748aac10bfc23e743a769fad
sechaser <413406236@qq.com>
2016-11-02 14:34:43 +0800

    55.Jump game

--- main.cpp
+++ main.cpp
@@ -5,72 +5,27 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+bool canJump(std::vector<int>& nums)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    int canArrive = 0;
+
+    //canArrive:目前为止能跳到的最远距离
+    //i+nums[i]:从当前一步出发能跳到的最远距离
+    for(std::vector<int>::size_type i = 0; i <= canArrive && i < nums.size(); ++ i)
+    {
+        if(i + nums[i] > canArrive)
+            canArrive = i + nums[i];
+    }
+
+    return canArrive >= nums.size() - 1;
+}
+
 
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::vector<int> nums = {3, 2, 1, 0, 4};
+    std::cout<<canJump(nums)<<std::endl;
 
     system("pause");
     return 0;
