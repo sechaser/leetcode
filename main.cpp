70cd6f64f736fd82825cff14f1342428be5c78ad
sechaser <413406236@qq.com>
2016-11-02 11:13:38 +0800

    54.Spiral matrix

--- main.cpp
+++ main.cpp
@@ -5,72 +5,62 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+std::vector<int> spiralOrder(std::vector<std::vector<int> >& matrix)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    std::vector<int> res;
+    if(matrix.empty())
+        return res;
 
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    int ring = (row > col) ? (col + 1)/2 : (row + 1)/2;
+    int cborder = col, rborder = row;
+
+    for(int i = 0; i != ring; ++ i, cborder -= 2, rborder -= 2)
+    {
+        for(int j = i; j != i + cborder; ++ j)
+            res.push_back(matrix[i][j]);
+        for(std::vector<int>::size_type i = 0; i != res.size(); ++ i)
+            std::cout<<std::setw(4)<<res[i];
+        std::cout<<std::endl;
+
+
+        for(int j = i+1; j != i + rborder; ++ j)
+            res.push_back(matrix[j][i + cborder - 1]);
+        for(std::vector<int>::size_type i = 0; i != res.size(); ++ i)
+            std::cout<<std::setw(4)<<res[i];
+        std::cout<<std::endl;
+
+
+        if(cborder == 1 || rborder == 1)
+            break;
+
+        for(int j = i + cborder - 2; j >= i; -- j)
+            res.push_back(matrix[i + rborder - 1][j]);
+        for(std::vector<int>::size_type i = 0; i != res.size(); ++ i)
+            std::cout<<std::setw(4)<<res[i];
+        std::cout<<std::endl;
+
+
+        for(int j = i + rborder - 2; j > i; -- j)
+            res.push_back(matrix[j][i]);
+        for(std::vector<int>::size_type i = 0; i != res.size(); ++ i)
+            std::cout<<std::setw(4)<<res[i];
+        std::cout<<std::endl;
+    }
+
+    return res;
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
+    std::vector<std::vector<int> > matrix{{1,2,3}, {4,5,6}, {7,8,9}};
+    std::vector<int> res = spiralOrder(matrix);
 
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    for(std::vector<int>::size_type i = 0; i != res.size(); ++ i)
+        std::cout<<std::setw(4)<<res[i];
+    std::cout<<std::endl;
 
     system("pause");
     return 0;
