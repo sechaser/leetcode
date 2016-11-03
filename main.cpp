d4d649f47d0f9f367911d9b9843789a62de0c281
sechaser <413406236@qq.com>
2016-11-03 22:01:42 +0800

    64.Minimum path sum

--- main.cpp
+++ main.cpp
@@ -5,72 +5,39 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+int minPathSum(std::vector<std::vector<int> >& grid)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    if(grid.empty())
+        return 0;
 
+    int m = grid.size(), n = grid[0].size();
+    std::vector<int> dp(n, 0);
+    for(int i = 0; i != n; ++ i)
+    {
+        if(i == 0)
+            dp[i] = grid[0][i];
+        else
+            dp[i] = dp[i-1] + grid[0][i];
+    }
+
+    for(int i = 1; i != m; ++ i)
+    {
+        for(int j = 0; j != n; ++ j)
+        {
+            if(j > 0)
+                dp[j] = std::min(grid[i][j] + dp[j-1], grid[i][j] + dp[j]);
+            else
+                dp[j] = dp[j] + grid[i][j];
+        }
+    }
+
+    return dp[n-1];
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::vector<std::vector<int> > grid{std::vector<int>{1,4,3}, std::vector<int>{3,1,1}, std::vector<int>{5,100,1}};
+    std::cout<<minPathSum(grid)<<std::endl;
 
     system("pause");
     return 0;
