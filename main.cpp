d222024ede0fc24781a24916cc26527e1c7fbda2
sechaser <413406236@qq.com>
2016-11-03 21:31:41 +0800

    63.Unique paths II

--- main.cpp
+++ main.cpp
@@ -5,72 +5,37 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
 
+int uniquePathsWithObstacles(std::vector<std::vector<int> >& obstacleGrid)
+{
+    if(obstacleGrid.empty())
+        return 0;
+
+    int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+    std::vector<int> dp(n, 0);
+    dp[0] = 1;
+
+    for(int i = 0; i != m; ++ i)
+    {
+        for(int j = 0; j != n; ++ j)
+        {
+            if(obstacleGrid[i][j] == 1)
+                dp[j] = 0;
+            else
+            {
+                if(j > 0)
+                    dp[j] = dp[j] + dp[j-1];
+            }
+        }
+    }
+
+    return dp[n-1];
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::vector<std::vector<int> > obstacleGrid{std::vector<int>{0,0,0}, std::vector<int>{0,1,0}, std::vector<int>{0,0,0}};
+    std::cout<<uniquePathsWithObstacles(obstacleGrid)<<std::endl;
 
     system("pause");
     return 0;
