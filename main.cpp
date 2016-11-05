9d51896175e3e82beaf957b5857238002ec029c8
sechaser <413406236@qq.com>
2016-11-05 20:02:48 +0800

    65.Valid number

--- main.cpp
+++ main.cpp
@@ -5,72 +5,64 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+bool isNumber(std::string s)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    bool num = false, numAfterE = true, dot = false, exp = false, sign = false;
+    int n = s.size();
 
+    for(int i = 0; i < n; ++ i)
+    {
+        //Space
+        std::cout<<"s[i] = "<<s[i]<<std::endl;
+        if(s[i] == ' ')
+        {
+            if(i < n - 1 && s[i+1] != ' ' && (num || dot || exp || sign))
+                return false;
+            std::cout<<"num="<<num<<",numAfterE="<<numAfterE<<",dot="<<dot<<",exp="<<exp<<",sign="<<sign<<std::endl;
+        }
+        //Sign
+        else if(s[i] == '+' || s[i] == '-')
+        {
+            if(i > 0 && (s[i-1] != 'e') && s[i-1] != ' ')
+                return false;
+            sign = true;
+            std::cout<<"num="<<num<<",numAfterE="<<numAfterE<<",dot="<<dot<<",exp="<<exp<<",sign="<<sign<<std::endl;
+        }
+        //Digit
+        else if(s[i] >= '0' && s[i] <= '9')
+        {
+            num = true;
+            numAfterE = true;
+            std::cout<<"num="<<num<<",numAfterE="<<numAfterE<<",dot="<<dot<<",exp="<<exp<<",sign="<<sign<<std::endl;
+        }
+        //Dot
+        else if(s[i] == '.')
+        {
+            if(dot || exp)
+                return false;
+            dot = true;
+            std::cout<<"num="<<num<<",numAfterE="<<numAfterE<<",dot="<<dot<<",exp="<<exp<<",sign="<<sign<<std::endl;
+        }
+        //Exp
+        else if(s[i] == 'e' || s[i] == 'E')
+        {
+            if(exp || !num)
+                return false;
+            exp = true;
+            numAfterE = false;
+            std::cout<<"num="<<num<<",numAfterE="<<numAfterE<<",dot="<<dot<<",exp="<<exp<<",sign="<<sign<<std::endl;
+        }
+        else
+            return false;
+    }
+
+    return num && numAfterE;
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::string s = " 005047e+6";
+    std::cout<<isNumber(s)<<std::endl;
 
     system("pause");
     return 0;
