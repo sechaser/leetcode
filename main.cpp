84e003c83738474a63d90d4bbf28b98d9e44328d
sechaser <413406236@qq.com>
2016-11-05 21:40:06 +0800

    67.Add binary

--- main.cpp
+++ main.cpp
@@ -5,72 +5,98 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+std::string addBinary(std::string a, std::string b)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    if(a.empty())
+        return b;
+    else if(b.empty())
+        return a;
+    else
+    {
+        std::string res;
+        if(a.size() < b.size())
+        {
+            std::string tmp = b;
+            b = a;
+            a = tmp;
+        }
+        std::string::size_type asz = a.size(), bsz = b.size();
+        int gap = asz - bsz;
 
+        bool carry = 0;
+        for(int i = bsz - 1; i >= 0; -- i)
+        {
+            if(a[gap + i] == '1' && b[i] == '1')
+            {
+                if(carry)
+                    res.insert(res.begin(), '1');
+                else
+                {
+                    res.insert(res.begin(), '0');
+                    carry = 1;
+                }
+            }
+            else if(a[gap + i] == '0' && b[i] == '0')
+            {
+                if(carry)
+                {
+                    res.insert(res.begin(), '1');
+                    carry = 0;
+                }
+                else
+                    res.insert(res.begin(), '0');
+            }
+            else
+            {
+                if(carry)
+                    res.insert(res.begin(), '0');
+                else
+                    res.insert(res.begin(), '1');
+            }
+        }
+
+
+        if(carry)
+        {
+            for(int i = asz - bsz - 1; i >= 0; -- i)
+            {
+                if(a[i] == '1')
+                {
+                    if(carry)
+                        res.insert(res.begin(), '0');
+                    else
+                        res.insert(res.begin(), '1');
+                }
+                else
+                {
+                    if(carry)
+                    {
+                        res.insert(res.begin(), '1');
+                        carry = 0;
+                    }
+                    else
+                        res.insert(res.begin(), '0');
+                }
+            }
+        }
+        else
+        {
+            for(int i = asz - bsz - 1; i >= 0; -- i)
+                res.insert(res.begin(), a[i]);
+        }
+
+        if(carry)
+            res.insert(res.begin(), '1');
+
+        return res;
+    }
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::string a = "1010", b = "1011";
+    std::string res = addBinary(a, b);
+    std::cout<<res<<std::endl;
 
     system("pause");
     return 0;
