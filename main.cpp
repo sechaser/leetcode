c9f62c0aa6bd9a42d64ad574734a76148e7f7d7d
sechaser <413406236@qq.com>
2016-11-06 15:41:21 +0800

    70.Climbing stairs

--- main.cpp
+++ main.cpp
@@ -5,71 +5,58 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+//***************************************Method 1 : Time limit error******************************
+//void solver(int& ways, int n)
+//{
+//    if(n == 0)
+//    {
+//        ++ ways;
+//        return;
+//    }
+//    if(n < 0)
+//        return ;
 
+//    -- n;
+//    solver(ways, n);
+//    ++ n;
+
+//    n -= 2;
+//    solver(ways, n);
+//    n += 2;
+//}
+
+//*********************************************Method 2*******************************************
+//int climbStairs(int n)
+//{
+//    std::vector<int> dp(n+1, 0);
+//    dp[0] = 1;
+//    dp[1] = 1;
+
+//    for(int i = 2; i <= n; ++ i)
+//        dp[i] = dp[i-1] + dp[i-2];
+
+//    return dp[n];
+//}
+
+
+int climbStairs(int n)
+{
+    int step0 = 1, step1 = 1;
+    int step2;
+
+    for(int i = 2; i <= n; ++ i)
+    {
+        step2 = step0 + step1;
+        step0 = step1;
+        step1 = step2;
+    }
+
+    return step1;
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
+    std::cout<<climbStairs(4)<<std::endl;
 
 
     system("pause");
