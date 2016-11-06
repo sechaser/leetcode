334385f9792d820f203bf78ba170741c0033d86c
sechaser <413406236@qq.com>
2016-11-06 11:49:11 +0800

    68.Test justification

--- main.cpp
+++ main.cpp
@@ -5,72 +5,76 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+std::string createLine(std::vector<std::string>& words, int L, int start, int end, int totLen, bool isLast)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    std::string res;
+    if(start < 0 || end >= words.size() || start > end)
+        return res;
+    res.append(words[start]);
 
+    int num = end - start + 1;    //The number of words in this line
+
+    //This line only has one word or it is the last line
+    if(num == 1 || isLast)
+    {
+        for(int i = start + 1; i <= end; ++ i)
+            res.append(" " + words[i]);
+
+        int j = L - totLen - (num-1);
+        res.append(j, ' ');
+        return res;
+    }
+
+    int k = (L - totLen) / (num-1), m = (L - totLen) % (num-1);
+    for(int i = start + 1; i <= end; ++ i)
+    {
+        int nspace = i - start <= m ? k+1 : k;
+        res.append(nspace, ' ');
+        res.append(words[i]);
+    }
+
+    return res;
+}
+
+std::vector<std::string> fullJustify(std::vector<std::string>& words, int maxWidth)
+{
+    int start = 0, end = -1, totLen = 0;
+
+    std::vector<std::string> res;
+    int i = 0;
+
+    while(i < words.size())
+    {
+        int newLen = totLen + (end - start + 1) + words[i].size();
+        if(newLen <= maxWidth)
+        {
+            end = i;
+            totLen += words[i].size();
+            ++ i;
+        }
+        else
+        {
+            std::string line = createLine(words, maxWidth, start, end, totLen, false);
+            res.push_back(line);
+            start = i;
+            end   = i - 1;
+            totLen = 0;
+        }
+    }
+
+    std::string lastLine = createLine(words, maxWidth, start, end, totLen, true);
+    res.push_back(lastLine);
+
+    return res;
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
+    std::vector<std::string> words{"This", "is", "an", "example", "of", "text", "justification."};
+    std::vector<std::string> res = fullJustify(words, 16);
 
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    for(std::vector<std::string>::size_type i = 0; i != res.size(); ++ i)
+        std::cout<<res[i]<<std::endl;
 
     system("pause");
     return 0;
