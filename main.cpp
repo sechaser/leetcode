75168b41be596b186b45215fe5036a41b61be781
sechaser <413406236@qq.com>
2016-11-07 18:53:02 +0800

    72.Edit distance

--- main.cpp
+++ main.cpp
@@ -5,71 +5,36 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+#include <math.h>
 
+int minDistance(std::string word1, std::string word2)
+{
+    int sz1 = word1.size(), sz2 = word2.size();
+    int dp[sz1+1][sz2+1];
+
+    for(int i = 0; i <= sz1; ++ i)
+        dp[i][0] = i;
+    for(int i = 0; i <= sz2; ++ i)
+        dp[0][i] = i;
+
+    for(int i = 1; i <= sz1; ++ i)
+    {
+        for(int j = 1; j <= sz2; ++ j)
+        {
+            if(word1[i-1] == word2[j-1])
+                dp[i][j] = dp[i-1][j-1];
+            else
+                dp[i][j] = std::min(dp[i-1][j-1], std::min(dp[i-1][j], dp[i][j-1])) + 1;
+        }
+    }
+
+    return dp[sz1][sz2];
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
+    std::string word1 = "a", word2 = "b";
+    std::cout<<minDistance(word1, word2)<<std::endl;
 
 
     system("pause");
