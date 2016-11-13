9c9bac170029d0ead6ab11df1cad97d4e4f8d96f
sechaser <413406236@qq.com>
2016-11-13 10:03:00 +0800

    77.Combinations

--- main.cpp
+++ main.cpp
@@ -5,70 +5,45 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+
+void solver(std::vector<std::vector<int> >& res, std::vector<int>& tmp, int prev, int n, int k)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    if(k == 0)
+    {
+        res.push_back(tmp);
+        return;
+    }
+
+    for(int i = prev + 1; i <= n; ++ i)
+    {
+        tmp.push_back(i);
+        solver(res, tmp, i, n, -- k);
+        tmp.pop_back();
+        ++ k;
+    }
+}
+
+std::vector<std::vector<int> > combine(int n , int k)
+{
+    std::vector<std::vector<int> > res;
+    std::vector<int> tmp;
+
+    solver(res, tmp, 0, n, k);
+
+    return res;
+}
+
 
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
+    std::vector<std::vector<int> > res = combine(4, 3);
+
+    for(std::vector<std::vector<int> >::size_type i = 0; i != res.size(); ++ i)
     {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
+        for(std::vector<int>::size_type j = 0; j != res[i].size(); ++ j)
+            std::cout<<std::setw(4)<<res[i][j];
+        std::cout<<std::endl;
     }
 
 
