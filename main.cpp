e270b7cc9ca15c38c3db492c647e4abbc4fd5561
sechaser <413406236@qq.com>
2016-11-14 19:14:21 +0800

    82.Remove duplicates from sorted list II

--- main.cpp
+++ main.cpp
@@ -13,13 +13,65 @@ struct ListNode
 };
 
 
+ListNode* deleteDuplicates(ListNode* head)
+{
+    if(head == NULL)
+        return head;
+
+    ListNode* node = (ListNode *)malloc(sizeof(ListNode));
+    node->next = head;
+    node->val  = INT_MAX;
+
+    ListNode* curr = head->next;
+    ListNode* prev = head;
+    ListNode* pp = node;
+
+    while(curr != NULL)
+    {
+        while(curr != NULL && prev->val == curr->val)
+            curr = curr->next;
+
+        //Duplicate
+        if(prev->next != curr)
+        {
+            if(curr == NULL)
+            {
+                pp->next = NULL;
+                return node->next;
+            }
+            else
+            {
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        else
+        {
+            pp->next = prev;
+            pp = prev;
+
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+
+    pp->next = prev;
+
+    return node->next;
+}
+
+
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
+    ListNode* node, *p, *lis1;
     for(int i = 0; i != 5; ++ i)
     {
         node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
+        if(i == 1)
+            node->val = 0;
+        else
+            node->val = i;
+
         node->next = NULL;
 
         if(i == 0)
@@ -34,45 +86,21 @@ int main()
         }
     }
 
-
-    for(int i = 0; i != 5; ++ i)
+    p = deleteDuplicates(lis1);
+    while(p != NULL)
     {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
+        std::cout<<p->val<<std::endl;
+        p = p->next;
     }
 
 
     system("pause");
     return 0;
 }
+
+
+//Case 1: 1->1->1->1->1->NULL
+//Case 2: 0->1->2->3->4->NULL
+//Case 3: 0->0->2->2->4->NULL
+//Case 4: 0->0->2->3->4->NULL
 
