cdf71a145d30cfee866364379ecc1acdcac98ed6
sechaser <413406236@qq.com>
2016-11-14 16:53:37 +0800

    81.Search in rotated sorted array II

--- main.cpp
+++ main.cpp
@@ -5,72 +5,50 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+bool search(std::vector<int>& nums, int target)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    if(nums.empty())
+        return false;
 
+    int sz = nums.size();
+    int l = 0, r = sz - 1;
+    int m;
+
+    while(l <= r)
+    {
+        m = (l + r) / 2;
+
+        if(nums[m] == target)
+            return true;
+
+        //Left part is sorted
+        if(nums[l] < nums[m])
+        {
+            if(nums[l] <= target && target < nums[m])
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        //Right part is sorted
+        else if (nums[l] > nums[m])
+        {
+            if(nums[m] < target && target <= nums[r])
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+        else
+            ++ l;
+    }
+
+    return false;
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
+    std::vector<int> nums{1, 3, 1, 1, 1};
 
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::cout<<search(nums, 3)<<std::endl;
 
     system("pause");
     return 0;
