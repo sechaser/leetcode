53c048d52ccfa3633e79162d4ff2a5fece4f19e4
sechaser <413406236@qq.com>
2016-11-15 15:11:59 +0800

    88.Merge sorted array

--- main.cpp
+++ main.cpp
@@ -4,73 +4,47 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <iterator>
 
-struct ListNode
+void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    nums1.erase(nums1.begin() + m, nums1.end());
+    std::vector<int>::iterator it1 = nums1.begin(), it2 = nums2.begin();
 
+    while(m > 0 && n > 0)
+    {
+        if(*it1 <= *it2)
+        {
+            ++ it1;
+            -- m;
+        }
+        else
+        {
+            it1 = nums1.insert(it1, *it2);
+            ++ it1;
+            ++ it2;
+            -- n;
+        }
+    }
+
+    while(n > 0)
+    {
+        nums1.push_back(*it2);
+        ++ it2;
+        -- n;
+    }
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
+    std::vector<int> nums1{4, 0, 0, 0, 0, 0};
+    std::vector<int> nums2{1, 2, 3, 5, 6};
 
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
+    merge(nums1, 1, nums2, 5);
 
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    for(std::vector<int>::size_type i = 0; i != nums1.size(); ++ i)
+        std::cout<<std::setw(4)<<nums1[i];
+    std::cout<<std::endl;
 
     system("pause");
     return 0;
