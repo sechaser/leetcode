537955501a05efa6979a751e28343d02ba7df322
sechaser <413406236@qq.com>
2016-11-15 14:24:00 +0800

    Scramble string

--- main.cpp
+++ main.cpp
@@ -5,72 +5,50 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+bool isScramble(std::string s1, std::string s2)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    if(s1.size() != s2.size())
+        return false;
+    if(s1.empty())
+        return true;
 
+    std::vector<std::vector<std::vector<bool> > > dp(s1.size(), std::vector<std::vector<bool> >(s2.size(), std::vector<bool>(s1.size()+1, 0)));
+    for(int i = 0; i != s1.size(); ++ i)
+    {
+        for(int j = 0; j != s2.size(); ++ j)
+        {
+            if(s1[i] == s2[j])
+                dp[i][j][1] = 1;
+            else
+                dp[i][j][1] = 0;
+        }
+    }
+
+    for(int len = 2; len <= s1.size(); ++ len)
+    {
+        for(int i = 0; i != s1.size()-len+1; ++ i)
+        {
+            for(int j = 0; j != s2.size()-len+1; ++ j)
+            {
+                for(int k = 1; k != len; ++ k)
+                {
+                    if((dp[i][j][k]&&dp[i+k][j+k][len-k]) || (dp[i][j+len-k][k]&&(dp[i+k][j][len-k])))
+                    {
+                        dp[i][j][len] = 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    return dp[0][0][s1.size()];
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::string s1 = "great", s2 = "rgeat";
+    std::cout<<isScramble(s1, s2)<<std::endl;
 
     system("pause");
     return 0;
