2e6a226554ac690cb74ab9f117d01d433efcf62a
sechaser <413406236@qq.com>
2016-11-15 18:32:37 +0800

    91.Decode ways

--- main.cpp
+++ main.cpp
@@ -5,72 +5,37 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+int numDecodings(std::string s)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    int sz = s.size();
+    if(sz == 0)
+        return 0;
 
+    std::vector<int> dp(s.size() + 1, 0);
+    dp[0] = 1;
+    if(s[0] != '0')
+        dp[1] = 1;
+    else
+        dp[1] = 0;
+
+    for(int i = 2; i <= sz; ++ i)
+    {
+        if(s[i-1] != '0')
+            dp[i] = dp[i-1];
+        else
+            dp[i] = 0;
+
+        if(s[i-2] == '1' || (s[i-2] == '2' && (s[i-1] <= '6')))
+            dp[i] += dp[i-2];
+    }
+
+    return dp[sz];
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::string s = "17";
+    std::cout<<numDecodings(s)<<std::endl;
 
     system("pause");
     return 0;
