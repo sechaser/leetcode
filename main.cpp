baa2f3a15e80dd407507d6a92b4201dc1a26a00d
sechaser <413406236@qq.com>
2016-11-21 18:42:00 +0800

    100.Same tree

--- main.cpp
+++ main.cpp
@@ -15,51 +15,69 @@ struct TreeNode
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-void solver(std::vector<int>& res, TreeNode* root)
-{
-    if(root != NULL)
-    {
-        solver(res, root->left);
-        res.push_back(root->val);
-        solver(res, root->right);
-    }
-}
 
-std::vector<int> inorderTraversal(TreeNode* root)
+bool isSameTree(TreeNode* p, TreeNode* q)
 {
-    std::vector<int> res;
-    solver(res, root);
-    return res;
+    if(p == NULL && q == NULL)
+        return true;
+
+    if(p == NULL || q == NULL)
+        return false;
+
+    if(p->val != q->val)
+        return false;
+
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 }
 
 int main()
 {
+    //First tree
     TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
     node->val = 1;
     node->left = NULL;
     node->right = NULL;
 
-    TreeNode* root = node;
+    TreeNode* root1 = node;
 
     node = (TreeNode*)malloc(sizeof(TreeNode));
     node->val = 2;
     node->left = NULL;
     node->right = NULL;
 
-    root->right = node;
+    root1->right = node;
 
     node = (TreeNode*)malloc(sizeof(TreeNode));
     node->val = 3;
     node->left = NULL;
     node->right = NULL;
 
-    root->right->left = node;
+    root1->right->left = node;
 
-    std::vector<int> res = inorderTraversal(root);
+    //Second tree
+    node = (TreeNode*)malloc(sizeof(TreeNode));
+    node->val = 1;
+    node->left = NULL;
+    node->right = NULL;
 
-    for(std::vector<int>::size_type i = 0; i != res.size(); ++ i)
-        std::cout<<std::setw(4)<<res[i];
-    std::cout<<std::endl;
+    TreeNode* root2 = node;
+
+    node = (TreeNode*)malloc(sizeof(TreeNode));
+    node->val = 2;
+    node->left = NULL;
+    node->right = NULL;
+
+    root2->right = node;
+
+    node = (TreeNode*)malloc(sizeof(TreeNode));
+    node->val = 3;
+    node->left = NULL;
+    node->right = NULL;
+
+    root2->right->right = node;
+
+
+    std::cout<<isSameTree(root1, root2)<<std::endl;
 
     system("pause");
     return 0;
