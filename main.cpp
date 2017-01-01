15b4ac3c1f33976a97da9bdf7bc76245da757a21
sechaser <413406236@qq.com>
2017-01-01 16:04:57 +0800

    123.Best time to buy and sell stock III

--- main.cpp
+++ main.cpp
@@ -5,71 +5,83 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+//Method 1 : run time error
+//int maxProfit(std::vector<int>& prices)
+//{
+//    if(prices.empty())
+//        return 0;
 
+//    std::vector<int> res;
+//    std::vector<int>::size_type sz = prices.size();
+
+//    prices.push_back(-1);
+//    int i = 0;
+//    while(i != sz)
+//    {
+//        int beg = i;
+//        while(i != sz && prices[i+1] >= prices[i])
+//            ++ i;
+
+//        if(i != sz)
+//        {
+//            res.push_back(prices[i]-prices[beg]);
+//            ++ i;
+//        }
+//    }
+
+//    prices.pop_back();
+
+//    std::sort(res.begin(), res.end(), [](const int& lhs, const int& rhs){
+//        return lhs <= rhs;
+//    });
+
+//    if(res.size() == 1)
+//        return res[0];
+//    else
+//        return res[res.size()-1] + res[res.size()-2];
+//}
+
+
+int maxProfit(std::vector<int>& prices)
+{
+    if(prices.size() <= 1)
+        return 0;
+
+    std::vector<int>::size_type sz = prices.size();
+    std::vector<int> leftProfit(sz, 0);
+
+    int leftMaxProfit = 0, minPrice = prices[0];
+    for(int i = 1; i != sz; ++ i)
+    {
+        if(prices[i] < minPrice)
+            minPrice = prices[i];
+        else
+            leftMaxProfit = std::max(leftMaxProfit, prices[i] - minPrice);
+        leftProfit[i] = leftMaxProfit;
+    }
+
+    for(std::vector<int>::size_type i = 0; i != sz; ++ i)
+        std::cout<<std::setw(4)<<leftProfit[i];
+    std::cout<<std::endl;
+
+    int res = leftProfit[sz - 1];
+    int rightMaxProfit = 0, maxPrice = prices[sz - 1];
+    for(int i = (sz - 2); i >= 0; -- i)
+    {
+        if(prices[i] > maxPrice)
+            maxPrice = prices[i];
+        else
+            rightMaxProfit = std::max(rightMaxProfit, maxPrice - prices[i]);
+        res = std::max(res, rightMaxProfit + leftProfit[i]);
+    }
+
+    return res;
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
+    std::vector<int> prices{1, 2, 3, 5, 4, 6, 7};
+    std::cout<<maxProfit(prices)<<std::endl;
 
 
     system("pause");
