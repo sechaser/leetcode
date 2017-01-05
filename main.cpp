ea382c7513c8c161868ac8fe741c1956af6545bd
sechaser <413406236@qq.com>
2017-01-05 20:35:01 +0800

    127.Word Ladder

--- main.cpp
+++ main.cpp
@@ -4,73 +4,63 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <unordered_set>
+#include <queue>
 
-struct ListNode
+int ladderLength(std::string beginWord, std::string endWord, std::unordered_set<std::string>& wordList)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    std::queue<std::string> Q;
+    Q.push(beginWord);
+    Q.push("");
 
+    int res = 1;
+    while(!Q.empty())
+    {
+        std::string str = Q.front();
+        Q.pop();
+
+        if(str != "")
+        {
+            int strLen = str.length();
+            for(int i = 0; i != strLen; ++ i)
+            {
+                char tmp = str[i];         //Store the char going to be substituted.
+                for(char c = 'a'; c <= 'z'; ++ c)
+                {
+                    if(c == tmp)
+                        continue;
+                    str[i] = c;
+
+                    if(str == endWord)
+                        return res + 1;
+
+                    if(wordList.find(str) != wordList.end())
+                    {
+                        Q.push(str);
+                        wordList.erase(str);
+                    }
+                }
+
+                str[i] = tmp;              //Recover the char
+            }
+        }
+        else if(!Q.empty())
+        {
+            ++ res;
+            Q.push("");
+        }
+    }
+
+    return 0;
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
+    std::string beginWord = "hit";
+    std::string endWord   = "cog";
+    std::unordered_set<std::string> wordList({"hot", "dot", "dog", "lot", "log"});
 
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::cout<<ladderLength(beginWord, endWord, wordList)<<std::endl;
 
     system("pause");
     return 0;
