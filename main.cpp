b39e3a34e6dff836f6a59ba636bbd987309a3843
sechaser <413406236@qq.com>
2017-02-13 10:10:39 +0800

    126 Word Ladder II

--- main.cpp
+++ main.cpp
@@ -4,71 +4,136 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <iterator>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
 
-struct ListNode
+
+
+typedef std::unordered_set<std::string>::iterator HashIter;
+
+void constructResult(std::unordered_map<std::string, std::vector<std::string> >& prePath, std::vector<std::vector<std::string> >& res,
+                     std::vector<std::string>& tmpres, std::string& beginWord, std::string& endWord)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    if(beginWord == endWord)
+    {
+        std::reverse(tmpres.begin(), tmpres.end());
+        res.push_back(tmpres);
+        std::reverse(tmpres.begin(), tmpres.end());
+        return;
+    }
+
+    std::vector<std::string>& pre = prePath[endWord];
+    for(int i = 0; i != pre.size(); ++ i)
+    {
+        tmpres.push_back(pre[i]);
+        constructResult(prePath, res, tmpres, beginWord, pre[i]);
+        tmpres.pop_back();
+    }
+}
+
+std::vector<std::vector<std::string> > findLadders(std::string beginWord, std::string endWord, std::vector<std::string>& wordList)
+{
+    std::queue<std::string> Q;
+    Q.push(beginWord);
+    Q.push("");
+    bool hasFound = false;
+
+    std::unordered_map<std::string, std::vector<std::string> > prePath;
+    std::unordered_set<std::string> hashTable;
+
+    while(!Q.empty())
+    {
+        std::string str = Q.front(), strCopy = str;
+        Q.pop();
+
+        if(str != "")
+        {
+            int strLen = str.length();
+            for(int i = 0; i != strLen; ++ i)
+            {
+                char tmp = str[i];
+                for(char c = 'a'; c <= 'z'; ++ c)
+                {
+                    if(c == tmp)
+                        continue;
+                    str[i] = c;
+                    if(str == endWord)
+                    {
+                        hasFound = true;
+                        prePath[endWord].push_back(strCopy);
+                        goto END;
+                    }
+
+                    if(std::find(wordList.begin(), wordList.end(), str) != wordList.end())
+                    {
+                        prePath[str].push_back(strCopy);
+                        if(hashTable.find(str) == hashTable.end())
+                        {
+                            Q.push(str);
+                            hashTable.insert(str);
+                        }
+                    }
+                }
+
+                str[i] = tmp;
+            }
+        }
+        else if(!Q.empty())
+        {
+            if(hasFound)
+                break;
+
+            std::vector<std::string> tmp;
+            for(HashIter it = hashTable.begin(); it != hashTable.end(); ++ it)
+                tmp.push_back(*it);
+            hashTable.clear();
+
+            for(std::vector<std::string>::size_type i = 0; i != tmp.size(); ++ i)
+            {
+                std::vector<std::string>::iterator it = wordList.begin();
+                while(it != wordList.end())
+                {
+                    if(*it == tmp[i])
+                    {
+                        wordList.erase(it);
+                        break;
+                    }
+                    else
+                        ++ it;
+                }
+            }
+
+            Q.push("");
+        }
+        END: ;
+    }
+
+    std::vector<std::vector<std::string> > res;
+    if(prePath.find(endWord) == prePath.end())
+        return res;
+
+    std::vector<std::string> tmpres;
+    tmpres.push_back(endWord);
+
+    constructResult(prePath, res, tmpres, beginWord, endWord);
+    return res;
+}
+
+
 
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
+    std::string beginWord = "hit", endWord = "cog";
+    std::vector<std::string> wordList({"hot", "dot", "dog", "lot", "log"});
+    std::vector<std::vector<std::string> > res = findLadders(beginWord, endWord, wordList);
+    for(int i = 0; i != res.size(); ++ i)
     {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
+        for(int j = 0; j != res[i].size(); ++ j)
+            std::cout<<res[i][j]<<" ";
+        std::cout<<std::endl;
     }
 
 
