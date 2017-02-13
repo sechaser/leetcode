d3c32b68b489fb6e840b80a8043c6593ef24862d
sechaser <413406236@qq.com>
2017-02-13 16:24:52 +0800

    130.Surrounded Regions

--- main.cpp
+++ main.cpp
@@ -4,73 +4,92 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <stack>
+#include <utility>
 
-struct ListNode
+void fill(std::vector<std::vector<char> >& board, int i, int j, char oldc, char newc)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    int row = board.size(), col = board[0].size();
+    if(i < 0 || j < 0 || i >= row || j >= col || board[i][j] != oldc)
+        return;
 
+    std::stack<std::pair<int, int> > s;
+    s.push(std::make_pair(i, j));
+
+    while(!s.empty())
+    {
+        i = s.top().first;
+        j = s.top().second;
+        s.pop();
+
+        board[i][j] = newc;
+
+        if(i > 0 && board[i-1][j] == oldc)
+            s.push(std::make_pair(i-1, j));
+        if(i < row-1 && board[i+1][j] == oldc)
+            s.push(std::make_pair(i+1, j));
+        if(j > 0 && board[i][j-1] == oldc)
+            s.push(std::make_pair(i, j-1));
+        if(j < col-1 && board[i][j+1] == oldc)
+            s.push(std::make_pair(i, j+1));
+    }
+}
+
+void fillBoarders(std::vector<std::vector<char> >& board, char oldc, char newc)
+{
+    int row = board.size(), col = board[0].size();
+
+    for(int i = 0; i != row; ++ i)
+    {
+        if(board[i][0] == oldc)
+            fill(board, i, 0, oldc, newc);
+        if(board[i][col-1] == oldc)
+            fill(board, i, col-1, oldc, newc);
+    }
+
+    for(int i = 0; i != col; ++ i)
+    {
+        if(board[0][i] == oldc)
+            fill(board, 0, i, oldc, newc);
+        if(board[row-1][i] == oldc)
+            fill(board, row-1, i, oldc, newc);
+    }
+}
+
+void replace(std::vector<std::vector<char> >& board, char oldc, char newc)
+{
+    int row = board.size(), col = board[0].size();
+    for(int i = 0; i != row; ++ i)
+    {
+        for(int j = 0; j != col; ++ j)
+        {
+            if(board[i][j] == oldc)
+                board[i][j] = newc;
+        }
+    }
+}
+
+void solve(std::vector<std::vector<char> >& board)
+{
+    if(board.size() < 3 || board[0].size() < 3)
+        return;
+
+    fillBoarders(board, 'O', 'Y');
+    replace(board, 'O', 'X');
+    fillBoarders(board, 'Y', 'O');
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
+    std::vector<std::vector<char> > board{{'X','X','X','X'}, {'X','O','O','X'}, {'X','X','O','X'}, {'X','O','X','X'}};
+    solve(board);
+
+    for(int i = 0; i != board.size(); ++ i)
     {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
+        for(int j = 0; j != board[0].size(); ++ j)
+            std::cout<<std::setw(4)<<board[i][j];
+        std::cout<<std::endl;
     }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
 
     system("pause");
     return 0;
