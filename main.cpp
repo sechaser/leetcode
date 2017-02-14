c6012a3e82b1fe4afee683e43ea7caac9c627da2
sechaser <413406236@qq.com>
2017-02-14 11:05:16 +0800

    132.Palindrome Paritioning II

--- main.cpp
+++ main.cpp
@@ -5,72 +5,92 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
 
+/* Method 1: Time limit exceed */
+//void solver(std::string s, int pos, std::vector<std::vector<bool> >& dp, int& curLen, int& res)
+//{
+//    if(pos == s.size())
+//    {
+//        if(curLen < res)
+//            res = curLen;
+//        return;
+//    }
+
+//    for(int i = pos; i != s.size(); ++ i)
+//    {
+//        if(dp[pos][i])
+//        {
+//            ++ curLen;
+//            solver(s, i+1, dp, curLen, res);
+//            -- curLen;
+//        }
+//    }
+//}
+
+//int minCut(std::string s)
+//{
+//    if(s.size() == 0)
+//        return 0;
+
+//    int sz = s.size();
+
+//    //Each character is a palindrome, (sz-1) cuts.
+//    int res = sz - 1;
+//    std::vector<std::vector<bool> > dp(sz, std::vector<bool>(sz, 0));
+
+//    for(int i = sz-1; i >= 0; -- i)
+//    {
+//        for(int j = i; j < sz; ++ j)
+//        {
+//            if((s[i] == s[j]) && ((j-i<=2) || dp[i+1][j-1]))
+//                dp[i][j] = 1;
+//        }
+//    }
+
+//    int curLen = (-1);
+//    solver(s, 0, dp, curLen, res);
+
+//    return res;
+//}
+
+
+int minCut(std::string s)
+{
+    if(s.size() == 0)
+        return 0;
+
+    int sz = s.size();
+
+    //Each character is a palindrome, (sz-1) cuts.
+    std::vector<std::vector<bool> > dp(sz, std::vector<bool>(sz, 0));
+
+    for(int i = sz-1; i >= 0; -- i)
+    {
+        for(int j = i; j < sz; ++ j)
+        {
+            if((s[i] == s[j]) && ((j-i<=2) || dp[i+1][j-1]))
+                dp[i][j] = 1;
+        }
+    }
+
+    std::vector<int> res(sz+1, INT_MAX);
+    res[0] = -1;
+    for(int i = 1; i <= sz; ++ i)
+    {
+        for(int j = i-1; j >= 0; -- j)
+        {
+            if(dp[j][i-1])
+                res[i] = std::min(res[i], res[j] + 1);
+        }
+    }
+
+    return res[sz];
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::string s = "bbb";
+    std::cout<<minCut(s)<<std::endl;
 
     system("pause");
     return 0;
