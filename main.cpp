4e0a39c89d01262e7bba324e957a2903878f7e04
sechaser <413406236@qq.com>
2017-02-14 17:03:56 +0800

    133.Clone Graph

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <unordered_map>
+#include <queue>
 
 struct UndirectedGraphNode
 {
@@ -13,6 +15,64 @@ struct UndirectedGraphNode
 };
 
 
+/* DFS */
+//UndirectedGraphNode* solver(UndirectedGraphNode* node, std::unordered_map<int, UndirectedGraphNode*>& track)
+//{
+//    if(node == NULL)
+//        return NULL;
+
+//    if(track.count(node->label))
+//        return track[node->label];
+
+//    UndirectedGraphNode* new_node = new UndirectedGraphNode(node->label);
+//    new_node->neighbors.resize(node->neighbors.size());
+//    track[node->label] = new_node;
+
+//    for(int i = 0; i != node->neighbors.size(); ++ i)
+//        new_node->neighbors[i] = solver(node->neighbors[i], track);
+
+//    return new_node;
+//}
+
+//UndirectedGraphNode* cloneGraph(UndirectedGraphNode* node)
+//{
+//    std::unordered_map<int, UndirectedGraphNode*> track;
+//    return solver(node, track);
+//}
+
+/* BFS */
+UndirectedGraphNode* cloneGraph(UndirectedGraphNode* node)
+{
+    if(node == NULL)
+        return NULL;
+
+    std::unordered_map<int, UndirectedGraphNode*> mp;
+    UndirectedGraphNode* new_node = new UndirectedGraphNode(node->label);
+
+    mp[node->label] = new_node;
+    std::queue<UndirectedGraphNode*> toVisit;
+    toVisit.push(node);
+
+    while(!toVisit.empty())
+    {
+        UndirectedGraphNode* cur = toVisit.front();
+        toVisit.pop();
+
+        for(UndirectedGraphNode* neigh : cur->neighbors)
+        {
+            if(!mp.count(neigh->label))
+            {
+                UndirectedGraphNode* tmp = new UndirectedGraphNode(neigh->label);
+                mp[neigh->label] = tmp;
+                toVisit.push(neigh);
+            }
+            mp[cur->label]->neighbors.push_back(mp[neigh->label]);
+        }
+    }
+
+    return new_node;
+}
+
 int main()
 {
     UndirectedGraphNode a(0);
@@ -21,9 +81,10 @@ int main()
 
     a.neighbors.push_back(&b);
     a.neighbors.push_back(&c);
-
     b.neighbors.push_back(&c);
     c.neighbors.push_back(&c);
+
+    UndirectedGraphNode* res = cloneGraph(&a);
 
     system("pause");
     return 0;
