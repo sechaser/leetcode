bd53d170669813e97892273f74fff39eae750e1c
sechaser <413406236@qq.com>
2017-02-21 18:02:56 +0800

    Add one solution

--- main.cpp
+++ main.cpp
@@ -2,27 +2,49 @@
 #include <vector>
 #include <algorithm>
 
+//int maxArea(std::vector<int>& height)
+//{
+//    int area = 0;
+//    int maxArea = 0;
+//    std::vector<int>::size_type i = 0, j = height.size() - 1;
+
+//    while(i < j)
+//    {
+//        area = (j - i) * std::min(height[i], height[j]);
+
+//        if(height[i] < height[j])
+//            ++ i;
+//        else
+//            -- j;
+
+//        if(area > maxArea)
+//            maxArea = area;
+//    }
+
+//    return maxArea;
+//}
+
+
 int maxArea(std::vector<int>& height)
 {
-    int area = 0;
-    int maxArea = 0;
+    int water = 0;
     std::vector<int>::size_type i = 0, j = height.size() - 1;
+    int h;
 
     while(i < j)
     {
-        area = (j - i) * std::min(height[i], height[j]);
+        h = std::min(height[i], height[j]);
+        water = std::max(water, (j - i) * h);
 
-        if(height[i] < height[j])
+        while(height[i] <= h && i < j)
             ++ i;
-        else
+        while(height[j] <= h && i < j)
             -- j;
-
-        if(area > maxArea)
-            maxArea = area;
     }
 
     return maxArea;
 }
+
 
 int main()
 {
