5f5c0e7d915830a0957e9d1a15a6b4ada5b8ef0e
sechaser <413406236@qq.com>
2017-02-22 18:19:22 +0800

    Modify my code

--- main.cpp
+++ main.cpp
@@ -6,45 +6,18 @@
 #include <iomanip>
 #include <unordered_map>
 
-//std::vector<int> findSubstring(std::string s, std::vector<std::string>& words)
-//{
-//    std::vector<int> res;
-//    if(s.empty() || words.empty())
-//        return res;
+bool checkSubString(std::string s, int beg, std::vector<std::string>& words, std::unordered_map<std::string, int>& ht)
+{
+    std::unordered_map<std::string, int> tmpHt;
 
-//    size_t word_sz = words.size(), word_length = words[0].size();
-//    if(word_sz * word_length > s.size())
-//        return res;
+    for(int num = 0; num != words.size(); ++ num)
+    {
+        std::string str = s.substr(beg + words[0].size() * num, words[0].size());
+        ++ tmpHt[str];
+    }
 
-//    //Use words vector to build associate container.
-//    std::unordered_map<std::string, int> m1;
-//    for(auto &e : words)
-//        ++ m1[e];
-
-//    for(size_t i = 0; i <= (s.size() - word_sz * word_length); ++ i)
-//    {
-//        std::unordered_map<std::string, int> m2;
-//        int j;
-
-//        for(j = 0; j != word_sz; ++ j)
-//        {
-//            std::string t = s.substr(i + j * word_length, word_length);
-//            if(m1.find(t) == m1.end())
-//                break;
-
-//            ++ m2[t];
-
-//            //No repetition
-//            if(m2[t] > m1[t])
-//                break;
-//        }
-
-//        if(j == word_sz)
-//            res.push_back(i);
-//    }
-
-//    return res;
-//}
+    return tmpHt == ht;
+}
 
 std::vector<int> findSubstring(std::string s, std::vector<std::string>& words)
 {
@@ -52,57 +25,21 @@ std::vector<int> findSubstring(std::string s, std::vector<std::string>& words)
     if(s.empty() || words.empty())
         return res;
 
-    std::string::size_type sz = s.size(), word_length = words[0].size();
-    std::vector<std::string>::size_type words_sz = words.size();
-    if(words_sz * word_length > sz)
+    int word_length = words[0].size();
+    int words_sz = words.size();
+
+    if(word_length * words_sz > s.size())
         return res;
 
-    std::unordered_map<std::string, int> m1;
-    for(auto &e : words)
-        ++ m1[e];
+    std::unordered_map<std::string, int> ht;
+    for(int i = 0; i != words_sz; ++ i)
+        ++ ht[words[i]];
 
-    for(size_t i = 0; i != word_length; ++ i)
+    int sz = s.size();
+    for(std::string::size_type i = 0; i <= (sz - word_length * words_sz); ++ i)
     {
-        int left = i, count = 0;
-        std::unordered_map<std::string, int> m2;
-
-        for(int j = i; j <= sz - word_length; j = j + word_length)
-        {
-            std::string t = s.substr(j, word_length);
-
-            if(m1.count(t))
-            {
-                ++ m2[t];
-                if(m2[t] <= m1[t])
-                    ++ count;
-                else
-                {
-                    while(m2[t] > m1[t])
-                    {
-                        std::string t1 = s.substr(left, word_length);
-                        -- m2[t1];
-
-                        if(m2[t1] < m1[t1])
-                            -- count;
-                        left = left + word_length;
-                    }
-                }
-
-                if(count == words_sz)
-                {
-                    res.push_back(left);
-                    -- m2[s.substr(left, word_length)];
-                    -- count;
-                    left = left + word_length;
-                }
-            }
-            else
-            {
-                m2.clear();
-                count = 0;
-                left = j + word_length;
-            }
-        }
+        if(checkSubString(s, i, words, ht))
+            res.push_back(i);
     }
 
     return res;
@@ -110,8 +47,8 @@ std::vector<int> findSubstring(std::string s, std::vector<std::string>& words)
 
 int main()
 {
-    std::string s = "a";
-    std::vector<std::string> words{"a", "a"};
+    std::string s = "abac";
+    std::vector<std::string> words{"a", "b"};
     std::vector<int> res = findSubstring(s, words);
 
     for(std::vector<int>::size_type i = 0; i != res.size(); ++ i)
