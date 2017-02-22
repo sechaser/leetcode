dcf3c5178acc86a0f1971d63b0989ab0810cffba
sechaser <413406236@qq.com>
2017-02-22 16:49:34 +0800

    Add one solution

--- main.cpp
+++ main.cpp
@@ -49,28 +49,61 @@
 //}
 
 
+//int divide(int diviend, int divisor)
+//{
+//    unsigned int divd = diviend, divs = divisor;
+//    if(diviend < 0)
+//        divd = (-divd);
+//    if(divisor < 0)
+//        divs = (-divs);
+
+//    unsigned int res = 0;
+//    while(divd >= divs)
+//    {
+//        long long a = divs;
+//        int i;
+
+//        for(i = 1; a <= divd; ++ i)
+//            a = (a << 1);
+
+//        res  = res + (1 << (i - 2));
+//        divd = divd - (divs << (i - 2));
+//    }
+
+//    return (diviend > 0 ^ divisor > 0) ? (-res):(res >= INT_MAX ? INT_MAX : res);
+//}
+
+
 int divide(int diviend, int divisor)
 {
-    unsigned int divd = diviend, divs = divisor;
-    if(diviend < 0)
-        divd = (-divd);
-    if(divisor < 0)
-        divs = (-divs);
+    if(divisor == 0)
+        return diviend >= 0 ? INT_MAX : INT_MIN;
 
-    unsigned int res = 0;
-    while(divd >= divs)
+    if(diviend == INT_MIN && (divisor == (-1)))
+        return INT_MAX;
+
+    bool flag = 0;
+    if((diviend < 0 && divisor > 0) || (diviend > 0 && divisor < 0))
+        flag = 1;
+
+    unsigned long long divid = std::abs((long long)diviend);
+    unsigned long long divis = std::abs((long long)divisor);
+
+    int res = 0;
+
+    while(divid >= divis)
     {
-        long long a = divs;
+        unsigned long long a = divis;
+
         int i;
+        for(i = 0; a <= divid; ++ i)
+            a <<= 1;
 
-        for(i = 1; a <= divd; ++ i)
-            a = (a << 1);
-
-        res  = res + (1 << (i - 2));
-        divd = divd - (divs << (i - 2));
+        res   += (1 << (i-1));
+        divid -= (divis << (i-1));
     }
 
-    return (diviend > 0 ^ divisor > 0) ? (-res):(res >= INT_MAX ? INT_MAX : res);
+    return flag ? -res : res;
 }
 
 int main()
