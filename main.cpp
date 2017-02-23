6fd9b00c309d236293a3440c80e7a6221d8ce99b
sechaser <413406236@qq.com>
2017-02-23 14:05:52 +0800

    Add one solution

--- main.cpp
+++ main.cpp
@@ -5,46 +5,73 @@
 #include <algorithm>
 #include <iomanip>
 
-int firstMissiongPositive(std::vector<int>& nums)
+//int firstMissiongPositive(std::vector<int>& nums)
+//{
+//    if(nums.empty())
+//        return 1;
+
+//    std::sort(nums.begin(), nums.end());
+//    size_t i = 0;
+
+//    while(i != nums.size())
+//    {
+//        if(nums[i] < 0)
+//            ++ i;
+//        else
+//            break;
+//    }
+
+//    if(nums[i] <= 1)
+//    {
+//        size_t j = i + 1;
+//        while(j != nums.size())
+//        {
+//            if((nums[j] == nums[j-1] + 1 ) || (nums[j] == nums[j - 1]))
+//            {
+//                ++ j;
+//                continue;
+//            }
+//            else
+//                break;
+//        }
+
+//        return nums[j-1]+1;
+//    }
+//    else
+//        return 1;
+//}
+
+
+
+int firstMissiongPositive(std::vector<int>& A)
 {
-    if(nums.empty())
-        return 1;
+    int i=0;
+    int n = A.size();
 
-    std::sort(nums.begin(), nums.end());
-    size_t i = 0;
-
-    while(i != nums.size())
+    while(i<n)
     {
-        if(nums[i] < 0)
-            ++ i;
+        if(A[i]!=i+1 && A[i]>0 && A[i]<=n && A[i]!=A[A[i]-1])
+            std::swap(A[i],A[A[i]-1]);
         else
-            break;
+            i++;
+    }
+    for(int i=0; i<n; i++) {
+        if(A[i]!=i+1) return i+1;
     }
 
-    if(nums[i] <= 1)
-    {
-        size_t j = i + 1;
-        while(j != nums.size())
-        {
-            if((nums[j] == nums[j-1] + 1 ) || (nums[j] == nums[j - 1]))
-            {
-                ++ j;
-                continue;
-            }
-            else
-                break;
-        }
-
-        return nums[j-1]+1;
-    }
-    else
-        return 1;
+    return n+1;
 }
+
 
 int main()
 {
-    std::vector<int> nums = {1};
+    std::vector<int> nums = {-2, 5, 3, -7, 1, 6, -8};
     int res = firstMissiongPositive(nums);
+
+    for(int i = 0; i != nums.size(); ++ i)
+        std::cout<<nums[i]<<" ";
+    std::cout<<std::endl;
+
     std::cout<<res<<std::endl;
 
     system("pause");
