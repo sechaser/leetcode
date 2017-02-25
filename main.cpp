3741737eebeb77d81a0e12c7f114d1bef3fbc0e0
sechaser <413406236@qq.com>
2017-02-25 20:00:15 +0800

    Add one solution

--- main.cpp
+++ main.cpp
@@ -5,45 +5,105 @@
 #include <algorithm>
 #include <iomanip>
 
-void setZeros(std::vector<std::vector<int> >& matrix)
+//void setZeros(std::vector<std::vector<int> >& matrix)
+//{
+//    if(matrix.empty())
+//        return;
+
+//    int m = matrix.size(), n = matrix[0].size();
+//    std::vector<std::vector<int> > dp;
+//    dp.reserve(m);
+
+//    for(int i = 0; i != m; ++ i)
+//    {
+//        std::vector<int> onerow;
+//        for(int j = 0; j != n; ++ j)
+//        {
+//            if(matrix[i][j] == 0)
+//                onerow.push_back(j);
+//        }
+//        dp.push_back(onerow);
+//    }
+//    std::cout<<"hello"<<std::endl;
+
+//    for(int i = 0; i != m; ++ i)
+//    {
+//        if(!dp[i].empty())
+//        {
+//            //set row to zeros
+//            for(int j = 0; j != n; ++ j)
+//                matrix[i][j] = 0;
+
+//            //set column to zeros
+//            for(int j = 0; j != m; ++ j)
+//                matrix[j][dp[i][0]] = 0;
+
+//            for(int j = 1; j < dp[i].size(); ++ j)
+//            {
+//                for(int k = 0; k != m; ++ k)
+//                    matrix[k][dp[i][j]] = 0;
+//            }
+//        }
+//    }
+//}
+
+void setZeroes(std::vector<std::vector<int> >& matrix)
 {
     if(matrix.empty())
         return;
 
-    int m = matrix.size(), n = matrix[0].size();
-    std::vector<std::vector<int> > dp;
-    dp.reserve(m);
+    int row = matrix.size(), col = matrix[0].size();
+    bool firstRow = false, firstCol = false;
 
-    for(int i = 0; i != m; ++ i)
+    for(int i = 0; i != row; ++ i)
     {
-        std::vector<int> onerow;
-        for(int j = 0; j != n; ++ j)
+        if(matrix[i][0] == 0)
+        {
+            firstCol = 1;
+            break;
+        }
+    }
+
+    for(int i = 0; i != col; ++ i)
+    {
+        if(matrix[0][i] == 0)
+        {
+            firstRow = 1;
+            break;
+        }
+    }
+
+    for(int i = 1; i != row; ++ i)
+    {
+        for(int j = 1; j != col; ++ j)
         {
             if(matrix[i][j] == 0)
-                onerow.push_back(j);
-        }
-        dp.push_back(onerow);
-    }
-    std::cout<<"hello"<<std::endl;
-
-    for(int i = 0; i != m; ++ i)
-    {
-        if(!dp[i].empty())
-        {
-            //set row to zeros
-            for(int j = 0; j != n; ++ j)
-                matrix[i][j] = 0;
-
-            //set col to zeros
-            for(int j = 0; j != m; ++ j)
-                matrix[j][dp[i][0]] = 0;
-
-            for(int j = 1; j < dp[i].size(); ++ j)
             {
-                for(int k = 0; k != m; ++ k)
-                    matrix[k][dp[i][j]] = 0;
+                matrix[i][0] = 0;
+                matrix[0][i] = 0;
             }
         }
+    }
+
+    for(int i = 1; i != row; ++ i)
+    {
+        for(int j = 1; j != col; ++ j)
+        {
+            if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                matrix[i][j] = 0;
+        }
+    }
+
+    if(firstRow)
+    {
+        for(int i = 0; i != col; ++ i)
+            matrix[0][i] = 0;
+    }
+
+    if(firstCol)
+    {
+        for(int i = 0; i != row; ++ i)
+            matrix[i][0] = 0;
     }
 }
 
