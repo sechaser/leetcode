df4b28106ddf9cf6587fd80af3d0edd94e59fd32
sechaser <413406236@qq.com>
2017-02-26 07:58:21 +0800

    Add one solution

--- main.cpp
+++ main.cpp
@@ -4,49 +4,66 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
-void swap(std::vector<int>& nums, int low, int high)
-{
-    int temp   = nums[low];
-    nums[low]  = nums[high];
-    nums[high] = temp;
-}
+//void swap(std::vector<int>& nums, int low, int high)
+//{
+//    int temp   = nums[low];
+//    nums[low]  = nums[high];
+//    nums[high] = temp;
+//}
 
-int partition(std::vector<int>& nums, int low, int high)
-{
-    int value = nums[low];
-    while(low < high)
-    {
-        while((low < high) && (nums[high] >= value))
-            -- high;
-        swap(nums, low, high);
+//int partition(std::vector<int>& nums, int low, int high)
+//{
+//    int value = nums[low];
+//    while(low < high)
+//    {
+//        while((low < high) && (nums[high] >= value))
+//            -- high;
+//        swap(nums, low, high);
 
-        while((low < high) && (nums[low] <= value))
-            ++ low;
-        swap(nums, low, high);
-    }
+//        while((low < high) && (nums[low] <= value))
+//            ++ low;
+//        swap(nums, low, high);
+//    }
 
-    return low;
-}
+//    return low;
+//}
 
-void quickSort(std::vector<int>& nums, int low, int high)
-{
-    if(low < high)
-    {
-        int location = partition(nums, low, high);
-        quickSort(nums, low, location - 1);
-        quickSort(nums, location + 1, high);
-    }
-}
+//void quickSort(std::vector<int>& nums, int low, int high)
+//{
+//    if(low < high)
+//    {
+//        int location = partition(nums, low, high);
+//        quickSort(nums, low, location - 1);
+//        quickSort(nums, location + 1, high);
+//    }
+//}
+
+//void sortColors(std::vector<int>& nums)
+//{
+//    if(nums.empty())
+//        return;
+
+//    int sz = nums.size();
+//    int low = 0, high = sz - 1;
+
+//    quickSort(nums, low, high);
+//}
+
 
 void sortColors(std::vector<int>& nums)
 {
-    if(nums.empty())
-        return;
+    int left = 0, right = nums.size() - 1;
+    int i = 0;
 
-    int sz = nums.size();
-    int low = 0, high = sz - 1;
-
-    quickSort(nums, low, high);
+    while(i <= right)
+    {
+        if(nums[i] == 0)
+            std::swap(nums[i++], nums[left++]);
+        else if(nums[i] == 1)
+            ++ i;
+        else
+            std::swap(nums[i], nums[right--]);
+    }
 }
 
 int main()
