be9431d84422abb22b9340cf4959d835e6009c52
sechaser <413406236@qq.com>
2017-03-02 20:09:33 +0800

    Add one solution

--- main.cpp
+++ main.cpp
@@ -22,6 +22,22 @@
 //}
 
 
+int singleNumber(std::vector<int>& nums)
+{
+    int ones = 0, twos = 0, xthrees = 0;
+
+    for(int i = 0; i != nums.size(); ++ i)
+    {
+        twos |= (ones & nums[i]);
+        ones ^= nums[i];
+        xthrees = ~(ones & twos);
+
+        ones &= xthrees;
+        twos &= xthrees;
+    }
+
+    return ones;
+}
 
 int main()
 {
