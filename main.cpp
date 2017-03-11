09df59f1bc4e7786223a087cabea2835146588ba
sechaser <413406236@qq.com>
2017-03-11 13:52:32 +0800

    149.Max Points on a Line

--- main.cpp
+++ main.cpp
@@ -4,14 +4,48 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <unordered_map>
 
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
+struct Point{
+    int x;
+    int y;
+
+    Point():x(0),y(0) {}
+    Point(int a, int b):x(a), y(b) {}
 };
 
+
+int maxPoints(std::vector<Point>& points)
+{
+    int maxPts = 0;
+
+    for(std::vector<int>::size_type i = 0; i != points.size(); ++ i)
+    {
+        int nSame = 0, nInf = 0, nMax = 0;
+        std::unordered_map<float, int> mp;
+
+        for(std::vector<int>::size_type j = i+1; j != points.size(); ++ j)
+        {
+            if(points[j].x == points[i].x)
+            {
+                if(points[j].y == points[j].y)
+                    ++ nSame;
+                else
+                    ++ nInf;
+                continue;
+            }
+
+            float slope = (float)(points[j].y - points[i].y)/(float)(points[j].x - points[i].x);
+            ++ mp[slope];
+            nMax = std::max(nMax, mp[slope]);
+        }
+
+        nMax = std::max(nMax, nInf) + nSame + 1;
+        maxPts = std::max(maxPts, nMax);
+    }
+
+    return maxPts;
+}
 
 int main()
 {
