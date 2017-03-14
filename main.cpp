88788a36724fa6138b76fa97b43133b99b2b1936
sechaser <413406236@qq.com>
2017-03-14 16:25:33 +0800

    160.Intersection of Two Linked Lists

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <unordered_set>
 
 struct ListNode
 {
@@ -12,6 +13,72 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+
+//ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+//{
+//    std::unordered_set<ListNode*> st;
+
+//    while(headA)
+//    {
+//        st.push(headA);
+//        headA = headA->next;
+//    }
+
+//    while(headB)
+//    {
+//        if(st.count(headB))
+//            return headB;
+//        else
+//            headB = headB->next;
+//    }
+
+//    return NULL;
+//}
+
+
+ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+{
+    if(!headA || !headB)
+        return NULL;
+
+    int lenA = 1;
+    int lenB = 1;
+
+    ListNode* pA = headA, *pB = headB;
+    while(pA->next)
+    {
+        ++ lenA;
+        pA = pA->next;
+    }
+
+    while(pB->next)
+    {
+        ++ lenB;
+        pB = pB->next;
+    }
+
+    if(pA != pB)
+        return NULL;
+
+    if(lenA > lenB)
+    {
+        for(int i = 1; i <= lenA - lenB; ++ i)
+            headA = headA->next;
+    }
+    else
+    {
+        for(int i = 1; i <= lenB - lenA; ++ i)
+            headB = headB->next;
+    }
+
+    while(headA != headB)
+    {
+        headA = headA->next;
+        headB = headB->next;
+    }
+
+    return headA;
+}
 
 int main()
 {
