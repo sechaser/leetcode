5f26ff6f4cab3a31f22226b45151d14c9843a04c
sechaser <413406236@qq.com>
2017-03-14 17:15:05 +0800

    328.Odd Even Linked List

--- main.cpp
+++ main.cpp
@@ -12,6 +12,38 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+ListNode* oddEvenList(ListNode* head)
+{
+    if(head == NULL || head->next == NULL)
+        return head;
+
+    ListNode* oddHead  = new ListNode(0);
+    ListNode* evenHead = new ListNode(0);
+    ListNode* p1 = oddHead, *p2 = evenHead;
+
+    bool isOdd = 1;
+    while(head)
+    {
+        if(isOdd)
+        {
+            p1->next = head;
+            p1 = p1->next;
+        }
+        else
+        {
+            p2->next = head;
+            p2 = p2->next;
+        }
+
+        isOdd = !isOdd;
+        head = head->next;
+    }
+
+    p2->next = NULL;
+    p1->next = evenHead->next;
+
+    return oddHead->next;
+}
 
 int main()
 {
