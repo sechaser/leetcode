1fae2900260dac6bcffa711228b308a0fa597e6e
sechaser <413406236@qq.com>
2017-03-15 15:35:43 +0800

    67.Add Binary

--- main.cpp
+++ main.cpp
@@ -5,119 +5,80 @@
 #include <algorithm>
 #include <iomanip>
 
-//std::string addBinary(std::string a, std::string b)
-//{
-//    if(a.empty())
-//        return b;
-//    else if(b.empty())
-//        return a;
-//    else
-//    {
-//        std::string res;
-//        if(a.size() < b.size())
-//        {
-//            std::string tmp = b;
-//            b = a;
-//            a = tmp;
-//        }
-//        std::string::size_type asz = a.size(), bsz = b.size();
-//        int gap = asz - bsz;
-
-//        bool carry = 0;
-//        for(int i = bsz - 1; i >= 0; -- i)
-//        {
-//            if(a[gap + i] == '1' && b[i] == '1')
-//            {
-//                if(carry)
-//                    res.insert(res.begin(), '1');
-//                else
-//                {
-//                    res.insert(res.begin(), '0');
-//                    carry = 1;
-//                }
-//            }
-//            else if(a[gap + i] == '0' && b[i] == '0')
-//            {
-//                if(carry)
-//                {
-//                    res.insert(res.begin(), '1');
-//                    carry = 0;
-//                }
-//                else
-//                    res.insert(res.begin(), '0');
-//            }
-//            else
-//            {
-//                if(carry)
-//                    res.insert(res.begin(), '0');
-//                else
-//                    res.insert(res.begin(), '1');
-//            }
-//        }
-
-
-//        if(carry)
-//        {
-//            for(int i = asz - bsz - 1; i >= 0; -- i)
-//            {
-//                if(a[i] == '1')
-//                {
-//                    if(carry)
-//                        res.insert(res.begin(), '0');
-//                    else
-//                        res.insert(res.begin(), '1');
-//                }
-//                else
-//                {
-//                    if(carry)
-//                    {
-//                        res.insert(res.begin(), '1');
-//                        carry = 0;
-//                    }
-//                    else
-//                        res.insert(res.begin(), '0');
-//                }
-//            }
-//        }
-//        else
-//        {
-//            for(int i = asz - bsz - 1; i >= 0; -- i)
-//                res.insert(res.begin(), a[i]);
-//        }
-
-//        if(carry)
-//            res.insert(res.begin(), '1');
-
-//        return res;
-//    }
-//}
-
-
 std::string addBinary(std::string a, std::string b)
 {
-    std::string res;
+    if(a.empty() && b.empty())
+        return a;
+
+    if(a.empty())
+        return b;
+    if(b.empty())
+        return a;
+
+    if(a.size() < b.size())
+    {
+        std::string tmp(a);
+        a = b;
+        b = tmp;
+    }
+
+    int sz = b.size();
+    std::string res(a.size() + 1, '0');
+
     std::reverse(a.begin(), a.end());
     std::reverse(b.begin(), b.end());
 
-    int n = std::max(a.size(), b.size());
     int carry = 0;
-    for(int i = 0; i < n; ++ i)
+    for(int i = 0; i < b.size(); ++ i)
     {
-        if(i < a.size())
-            carry += (a[i] - '0');
-        if(i < b.size())
-            carry += (b[i] - '0');
+        carry = carry + (a[i] - '0') + (b[i] - '0');
+        res[i] = carry % 2 + '0';
+        carry = carry / 2;
+    }
 
-        res.push_back(carry%2+'0');
-        carry /= 2;
+    for(int i = b.size(); i < a.size(); ++ i)
+    {
+        carry = carry + (a[i] - '0');
+        res[i] = carry % 2 + '0';
+        carry = carry / 2;
     }
 
     if(carry)
-        res.push_back('1');
+        res[a.size()] = carry + '0';
+    else
+        res.pop_back();
+
     std::reverse(res.begin(), res.end());
 
     return res;
 }
+
+
+//std::string addBinary(std::string a, std::string b)
+//{
+//    std::string res;
+//    std::reverse(a.begin(), a.end());
+//    std::reverse(b.begin(), b.end());
+
+//    int n = std::max(a.size(), b.size());
+//    int carry = 0;
+//    for(int i = 0; i < n; ++ i)
+//    {
+//        if(i < a.size())
+//            carry += (a[i] - '0');
+//        if(i < b.size())
+//            carry += (b[i] - '0');
+
+//        res.push_back(carry%2+'0');
+//        carry /= 2;
+//    }
+
+//    if(carry)
+//        res.push_back('1');
+//    std::reverse(res.begin(), res.end());
+
+//    return res;
+//}
 
 int main()
 {
