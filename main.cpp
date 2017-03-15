7bd1963d05a2d525c1e314069e4be192580846ad
sechaser <413406236@qq.com>
2017-03-15 15:15:33 +0800

    49.Group Anagrams

--- main.cpp
+++ main.cpp
@@ -8,71 +8,22 @@
 #include <unordered_map>
 #include <iterator>
 
-//Time:O(n), space:O(n)
-//std::vector<std::vector<std::string> > groupAnagrams(std::vector<std::string>& strs)
-//{
-//    std::vector<std::vector<std::string> > res;
-//    size_t strs_sz = strs.size();
-//    if(strs_sz < 2)
-//    {
-//        res.push_back(strs);
-//        return res;
-//    }
-
-//    std::vector<std::pair<int, std::string> > temp;
-//    for(std::vector<std::string>::size_type i = 0; i != strs_sz; ++ i)
-//        temp.push_back(std::pair<int, std::string>(i, strs[i]));
-
-//    for(std::vector<std::pair<int, std::string> >::size_type i = 0; i != temp.size(); ++ i)
-//        std::sort(temp[i].second.begin(), temp[i].second.end());
-
-//    std::sort(temp.begin(), temp.end(), [](const std::pair<int, std::string>& lhs, const std::pair<int, std::string>& rhs){
-//        return lhs.second < rhs.second;
-//    });
-
-//    std::string str;
-//    for(std::vector<std::pair<int, std::string> >::size_type i = 0; i != temp.size(); ++ i)
-//    {
-//        if(i == 0)
-//        {
-//            res.push_back(std::vector<std::string>{strs[temp[0].first]});
-//            str = temp[0].second;
-//        }
-//        else
-//        {
-//            if(temp[i].second == str)
-//                res[res.size() - 1].push_back(strs[temp[i].first]);
-//            else
-//            {
-//                str = temp[i].second;
-//                res.push_back(std::vector<std::string>{strs[temp[i].first]});
-//            }
-//        }
-//    }
-
-//    return res;
-//}
-
 std::vector<std::vector<std::string> > groupAnagrams(std::vector<std::string>& strs)
 {
-    std::unordered_map<std::string, std::vector<int> > hashTable;
-
-    std::string str;
-    for(std::vector<std::string>::size_type i = 0; i != strs.size(); ++ i)
-    {
-        str = strs[i];
-        std::sort(str.begin(), str.end());
-        hashTable[str].push_back(i);
-    }
-
     std::vector<std::vector<std::string> > res;
-    for(std::unordered_map<std::string, std::vector<int> >::iterator it = hashTable.begin(); it != hashTable.end(); ++ it)
+    if(strs.empty())
+        return res;
+
+    std::unordered_map<std::string, std::vector<std::string> > mp;
+    for(std::vector<std::vector<std::string> >::size_type i = 0; i != strs.size(); ++ i)
     {
-        std::vector<std::string> tmp;
-        for(std::vector<int>::size_type i = 0; i != it->second.size(); ++ i)
-            tmp.push_back(strs[it->second[i]]);
-        res.push_back(tmp);
+        std::string tmp = strs[i];
+        std::sort(tmp.begin(), tmp.end());
+        mp[tmp].push_back(strs[i]);
     }
+
+    for(std::unordered_map<std::string, std::vector<std::string> >::iterator it = mp.begin(); it != mp.end(); ++ it)
+        res.push_back(it->second);
 
     return res;
 }
