711f50cdb3f813514f352c1bd72c954d1ad67be7
sechaser <413406236@qq.com>
2017-03-16 10:18:34 +0800

    165.Compare Version Numbers

--- main.cpp
+++ main.cpp
@@ -12,64 +12,67 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+int compareVersion(std::string version1, std::string version2)
+{
+    int i = 0;
+    std::vector<int> res1, res2;
+    int start = 0;
+
+    while(i < version1.size())
+    {
+        while(i < version1.size() && version1[i] != '.')
+            ++ i;
+        std::string t = version1.substr(start, i-start);
+        res1.push_back(std::stoi(t));
+
+        i = i + 1;
+        start = i;
+    }
+
+    i = 0;
+    start = 0;
+
+    while(i < version2.size())
+    {
+        while(i < version2.size() && version2[i] != '.')
+            ++ i;
+        std::string t = version2.substr(start, i-start);
+        res2.push_back(std::stoi(t));
+
+        i = i + 1;
+        start = i;
+    }
+
+    int minSZ = std::min(res1.size(), res2.size());
+    for(i = 0; i < minSZ; ++ i)
+    {
+        if(res1[i] > res2[i])
+            return 1;
+        else if(res1[i] < res2[i])
+            return -1;
+        else
+            continue;
+    }
+
+    for(i = minSZ; i < res1.size(); ++ i)
+    {
+        if(res1[i] != 0)
+            return 1;
+    }
+
+    for(i = minSZ; i < res2.size(); ++ i)
+    {
+        if(res2[i] != 0)
+            return -1;
+    }
+
+    return 0;
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
+    std::string version1 = "1.0.1", version2 = "1";
+    std::cout<<compareVersion(version1, version2)<<std::endl;
 
 
     system("pause");
