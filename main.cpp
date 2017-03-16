ed8d118960f5039a7d412f0912993b4c0d71468f
sechaser <413406236@qq.com>
2017-03-16 11:37:12 +0800

    345.Reverse Vowels of a String

--- main.cpp
+++ main.cpp
@@ -12,65 +12,44 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+bool isVowel(char ch)
+{
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        return true;
+    return false;
+}
+
+std::string reverseVowels(std::string s)
+{
+    if(s.empty())
+        return s;
+
+    int left = 0, right = s.size() - 1;
+
+    while(left < right)
+    {
+        while(left < right && !isVowel(tolower(s[left])))
+            ++ left;
+
+        while(left < right && !isVowel(tolower(s[right])))
+            -- right;
+
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        ++ left;
+        -- right;
+    }
+
+    return s;
+}
 
 int main()
 {
-    ListNode* node, *p, *lis1, *lis2, *lis3;
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis1 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i+5;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis2 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
-    for(int i = 0; i != 5; ++ i)
-    {
-        node = (ListNode*)malloc(sizeof(ListNode));
-        node->val  = i;
-        node->next = NULL;
-
-        if(i == 0)
-        {
-            lis3 = node;
-            p    = node;
-        }
-        else
-        {
-            p->next = node;
-            p       = node;
-        }
-    }
-
+    std::string s = "leetcode";
+    std::string res = reverseVowels(s);
+    std::cout<<res<<std::endl;
 
     system("pause");
     return 0;
