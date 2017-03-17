7e45ac1a941f13bd9bea64c26f26b341e3cc4a68
sechaser <413406236@qq.com>
2017-03-17 14:15:06 +0800

    485.Max Consecutive Ones

--- main.cpp
+++ main.cpp
@@ -12,10 +12,35 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+int findMaxConsecutiveOnes(std::vector<int>& nums)
+{
+    int max1s = 0;
+    int cur1s = 0;
+
+    int i = 0;
+    while(i < nums.size())
+    {
+        while(i < nums.size() && nums[i] != 1)
+            ++ i;
+
+        while(i < nums.size() && nums[i] == 1)
+        {
+            ++ cur1s;
+            ++ i;
+        }
+
+        if(cur1s > max1s)
+            max1s = cur1s;
+        cur1s = 0;
+    }
+
+    return max1s;
+}
 
 int main()
 {
-
+    std::vector<int> nums{0, 0, 0, 0};
+    std::cout<<findMaxConsecutiveOnes(nums)<<std::endl;
 
     system("pause");
     return 0;
