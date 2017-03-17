5c3e87f48adf68afd75faac550812ea7e8bf68fa
sechaser <413406236@qq.com>
2017-03-17 14:07:59 +0800

    495.Teemo Attacking

--- main.cpp
+++ main.cpp
@@ -12,9 +12,29 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+int findPosionedDuration(std::vector<int>& timeSeries, int duration)
+{
+    if(timeSeries.empty())
+        return 0;
+
+    int res = 0;
+    for(int i = 1; i < timeSeries.size(); ++ i)
+    {
+        if(timeSeries[i] - timeSeries[i-1] < duration)
+            res += (timeSeries[i] - timeSeries[i-1]);
+        else
+            res += duration;
+    }
+
+    res += duration;
+
+    return res;
+}
 
 int main()
 {
+    std::vector<int> timeSeries{1, 3, 6};
+    std::cout<<findPosionedDuration(timeSeries, 3)<<std::endl;
 
 
     system("pause");
