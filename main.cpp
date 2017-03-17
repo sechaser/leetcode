4b7731d17929cdf6852617150b1b8fc14768747b
sechaser <413406236@qq.com>
2017-03-17 08:26:10 +0800

    114.Flatten Binary Tree to Linked List

--- main.cpp
+++ main.cpp
@@ -16,64 +16,35 @@ struct TreeNode
 };
 
 
-//*******************************************Method 1**************************************
-//void preorder(TreeNode* root, std::vector<TreeNode*>& nodeVec)
-//{
-//    if(root != NULL)
-//    {
-//        nodeVec.push_back(root);
-//        preorder(root->left, nodeVec);
-//        preorder(root->right, nodeVec);
-//    }
-//}
-
-//void flatten(TreeNode* root)
-//{
-//    if(root == NULL)
-//        return;
-
-//    std::vector<TreeNode*> nodeVec;
-//    preorder(root, nodeVec);
-
-//    size_t sz = nodeVec.size();
-//    for(std::vector<TreeNode*>::size_type i = 0; i != sz - 1; ++ i)
-//    {
-//        nodeVec[i]->left  = NULL;
-//        nodeVec[i]->right = nodeVec[i+1];
-//    }
-//    nodeVec[sz-1]->left  = NULL;
-//    nodeVec[sz-1]->right = NULL;
-//}
-
-
-
-//*****************************************Method 2********************************************
-//void flatten(TreeNode* root)
-//{
-//    if(root == NULL)
-//        return ;
-
-//    if(root->left != NULL)
-//        flatten(root->left);
-
-//    if(root->right != NULL)
-//        flatten(root->right);
-
-//    TreeNode* tmp = root->right;
-//    root->right = root->left;
-//    root->left = NULL;
-
-//    while(root->right != NULL)
-//        root = root->right;
-//    root->right = tmp;
-//}
-
-
-//*****************************************Method 3*********************************************
-void flatten(TreeNode *root)
+void preorder(TreeNode* root, std::vector<TreeNode*>& nodeVec)
 {
-
+    if(root != NULL)
+    {
+        nodeVec.push_back(root);
+        preorder(root->left, nodeVec);
+        preorder(root->right, nodeVec);
+    }
 }
+
+void flatten(TreeNode* root)
+{
+    if(root == NULL)
+        return;
+
+    std::vector<TreeNode*> nodeVec;
+    preorder(root, nodeVec);
+
+    size_t sz = nodeVec.size();
+    for(std::vector<TreeNode*>::size_type i = 0; i != sz - 1; ++ i)
+    {
+        nodeVec[i]->left  = NULL;
+        nodeVec[i]->right = nodeVec[i+1];
+    }
+    nodeVec[sz-1]->left  = NULL;
+    nodeVec[sz-1]->right = NULL;
+}
+
+
 
 int main()
 {
