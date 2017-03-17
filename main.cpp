b489af9b85cb69bdc3a0edc4d550c8207c7b9d86
sechaser <413406236@qq.com>
2017-03-17 16:06:03 +0800

    414.Third Maximum Number

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <set>
 
 struct ListNode
 {
@@ -12,10 +13,22 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+int thirdMax(std::vector<int>& nums)
+{
+    std::set<int> top3;
+    for (auto num : nums)
+    {
+        top3.insert(num);
+        if (top3.size() > 3)
+            top3.erase(top3.begin());
+    }
+    return top3.size() == 3 ? *top3.begin() : *top3.rbegin();
+}
 
 int main()
 {
-
+    std::vector<int> nums{1,2,3,4};
+    std::cout<<thirdMax(nums)<<std::endl;
 
     system("pause");
     return 0;
