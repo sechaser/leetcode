3828133435a34a6d39b220d733a75f7693d0f67b
sechaser <413406236@qq.com>
2017-03-17 13:56:33 +0800

    532.K-diff Pairs in an Array

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <unordered_set>
 
 struct ListNode
 {
@@ -12,10 +13,38 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+int findPairs(std::vector<int>& nums, int k)
+{
+    if(nums.size() < 2)
+        return 0;
+
+    std::sort(nums.begin(), nums.end());
+    int res = 0;
+
+    for(int i = 0; i < nums.size() - 1; ++ i)
+    {
+        if(i > 0 && nums[i-1] == nums[i])
+            continue;
+
+        for(int j = i + 1; j < nums.size(); ++ j)
+        {
+            if(j > i+1 && nums[j] == nums[j-1])
+                continue;
+            if(nums[j] - nums[i] == k)
+            {
+                ++ res;
+                break;
+            }
+        }
+    }
+
+    return res;
+}
 
 int main()
 {
-
+    std::vector<int> nums{1,3,1,5,4};
+    std::cout<<findPairs(nums, 0)<<std::endl;
 
     system("pause");
     return 0;
