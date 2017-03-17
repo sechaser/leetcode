937d8ddcca9d375fd2c5bd577f3f49b8b9ee9e6d
sechaser <413406236@qq.com>
2017-03-17 15:20:29 +0800

    442.Find All Duplicates in an Array

--- main.cpp
+++ main.cpp
@@ -12,10 +12,29 @@ struct ListNode
     ListNode(int x):val(x), next(NULL) {}
 };
 
+std::vector<int> findDuplicates(std::vector<int>& nums)
+{
+    std::vector<int> res;
+
+    for(int i = 0; i < nums.size(); ++ i)
+    {
+        int index = std::abs(nums[i]) - 1;
+        if(nums[index] < 0)
+            res.push_back(std::abs(nums[i]));
+        else
+            nums[index] = -nums[index];
+    }
+
+    return res;
+}
 
 int main()
 {
+    std::vector<int> nums{4, 3, 2, 7 ,8, 2, 3 ,1};
+    std::vector<int> res = findDuplicates(nums);
 
+    for(int i = 0; i < res.size(); ++ i)
+        std::cout<<res[i]<<std::endl;
 
     system("pause");
     return 0;
