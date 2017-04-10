6370e97b3c7e6fab0242a8407e92da97e46243b1
sechaser <413406236@qq.com>
2017-04-10 10:57:23 +0800

    121.Best Time to Buy and Sell Stock

--- main.cpp
+++ main.cpp
@@ -5,23 +5,40 @@
 #include <algorithm>
 #include <iomanip>
 
+//int maxProfit(std::vector<int>& prices)
+//{
+//    if(prices.empty())
+//        return 0;
+
+//    int minValue = prices[0];
+
+//    int res = 0;
+//    for(int i = 1; i != prices.size(); ++ i)
+//    {
+//        if(prices[i] - minValue > res)
+//            res = prices[i] - minValue;
+
+//        minValue = std::min(minValue, prices[i]);
+//    }
+
+//    return res;
+//}
+
+
 int maxProfit(std::vector<int>& prices)
 {
     if(prices.empty())
         return 0;
 
-    int minValue = prices[0];
+    int local = 0, global = 0;
 
-    int res = 0;
-    for(int i = 1; i != prices.size(); ++ i)
+    for(int i = 0; i < prices.size() - 1; ++ i)
     {
-        if(prices[i] - minValue > res)
-            res = prices[i] - minValue;
-
-        minValue = std::min(minValue, prices[i]);
+        local  = std::max(local + prices[i+1] - prices[i], 0);
+        global = std::max(global, local);
     }
 
-    return res;
+    return global;
 }
 
 
