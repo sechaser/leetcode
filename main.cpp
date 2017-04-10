3d8b5c6a3edebf7012bb2c6ff5c0044ab27b6257
sechaser <413406236@qq.com>
2017-04-10 08:32:29 +0800

    53.Maximum Subarray

--- main.cpp
+++ main.cpp
@@ -5,72 +5,42 @@
 #include <algorithm>
 #include <iomanip>
 
-//*****************************************Time:O(n^3)*************************************
-//int maxSubArray(std::vector<int>& nums)
-//{
-//    int res = INT_MIN;
-
-//    size_t nums_sz = nums.size();
-
-//    //Consider every element in nums vector
-//    for(std::vector<int>::size_type i = 0; i != nums_sz; ++ i)
-//    {
-//        //Consider different section of element nums[i]
-//        for(std::vector<int>::size_type j = i; j != nums_sz; ++ j)
-//        {
-//            int sum = 0;
-//            //Consider section [i, j], calculate its sum
-//            for(std::vector<int>::size_type k = i; k <= j; ++ k)
-//                sum += nums[k];
-
-//            if(sum > res)
-//                res = sum;
-//        }
-//    }
-
-//    return res;
-//}
-
-
-
-//***************************************Time:O(n^2)********************************************
-//int maxSubArray(std::vector<int>& nums)
-//{
-//    int res = INT_MIN;
-//    size_t nums_sz = nums.size();
-
-//    for(std::vector<int>::size_type i = 0; i != nums_sz; ++ i)
-//    {
-//        int sum = 0;
-//        for(std::vector<int>::size_type j = i; j != nums_sz; ++ j)
-//        {
-//            sum += nums[j];
-//            if(sum > res)
-//                res = sum;
-//        }
-//    }
-
-//    return res;
-//}
-
 //***************************************Time:O(n)***********************************************
+//int maxSubArray(std::vector<int>& nums)
+//{
+//    int res = INT_MIN, sum = -1;
+//    size_t nums_sz = nums.size();
+
+//    for(int i = 0; i != nums_sz; ++ i)
+//    {
+//        if(sum > 0)
+//            sum += nums[i];
+//        else
+//            sum = nums[i];
+
+//        if(sum > res)
+//            res = sum;
+//    }
+
+//    return res;
+//}
+
+
 int maxSubArray(std::vector<int>& nums)
 {
-    int res = INT_MIN, sum = -1;
-    size_t nums_sz = nums.size();
+    if(nums.empty())
+        return 0;
 
-    for(int i = 0; i != nums_sz; ++ i)
+    int global = nums[0];
+    int local  = nums[0];
+
+    for(int i = 1; i < nums.size(); ++ i)
     {
-        if(sum > 0)
-            sum += nums[i];
-        else
-            sum = nums[i];
-
-        if(sum > res)
-            res = sum;
+        local  = std::max(nums[i], local + nums[i]);
+        global = std::max(local, global);
     }
 
-    return res;
+    return global;
 }
 
 
