f5e2ad65e60defba9d0e6f2630b3712776ad1642
sechaser <413406236@qq.com>
2017-04-11 08:01:04 +0800

    152.Maximum Product Subarray

--- main.cpp
+++ main.cpp
@@ -5,17 +5,26 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+int maxProduct(std::vector<int>& nums)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    int global = nums[0];
+    int max_local = nums[0], min_local = nums[0];
 
+    for(int i = 1; i < nums.size(); ++ i)
+    {
+        int tmp = max_local;
+        max_local = std::max(std::max(max_local*nums[i], nums[i]), min_local*nums[i]);
+        min_local = std::min(std::min(min_local*nums[i], nums[i]), tmp*nums[i]);
+        global = std::max(max_local, global);
+    }
+
+    return global;
+}
 
 int main()
 {
-
+    std::vector<int> nums{-2, 3, -4};
+    std::cout<<maxProduct(nums)<<std::endl;
 
     system("pause");
     return 0;
