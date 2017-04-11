96b96b6673648fc2207aedc918874b63a13c9409
sechaser <413406236@qq.com>
2017-04-11 09:32:08 +0800

    221.Maximal Square

--- main.cpp
+++ main.cpp
@@ -5,17 +5,52 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+int maximalSquare(std::vector<std::vector<char> >& matrix)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    if(matrix.empty())
+        return 0;
 
+    int m = matrix.size(), n = matrix[0].size();
+    std::vector<std::vector<int> > dp(m, std::vector<int>(n, 0));
+    int bian = 0;
+
+    for(int i = 0; i < n; ++ i)
+    {
+        dp[0][i] = (matrix[0][i] == '0')? 0:1;
+        bian = std::max(bian, dp[0][i]);
+    }
+    for(int i = 0; i < m; ++ i)
+    {
+        dp[i][0] = (matrix[i][0] == '0')? 0:1;
+        bian = std::max(bian, dp[i][0]);
+    }
+
+    for(int i = 1; i < m; ++ i)
+    {
+        for(int j = 1; j < n; ++ j)
+        {
+            if(matrix[i][j] == '1')
+            {
+                dp[i][j] = std::min(std::min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                bian = std::max(bian, dp[i][j]);
+            }
+        }
+    }
+
+    for(int i = 0; i < m; ++ i)
+    {
+        for(int j = 0; j < n; ++ j)
+            std::cout<<dp[i][j]<<" ";
+        std::cout<<std::endl;
+    }
+
+    return bian*bian;
+}
 
 int main()
 {
-
+    std::vector<std::vector<char> > matrix{{'1','1','1'},{'1','1','1'},{'1','1','1'}};
+    std::cout<<maximalSquare(matrix)<<std::endl;
 
     system("pause");
     return 0;
