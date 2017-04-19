0693cb66dc5920ef383bb66bd7f3cec9a745a677
sechaser <413406236@qq.com>
2017-04-19 14:09:37 +0800

    168.Excel Sheet Column Title

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <unordered_map>
 
-
+std::string convertToTitle(int n)
+{
+    return n == 0 ? "" : convertToTitle((n-1)/26) + (char)((n-1)%26 + 'A');
+}
 
 int main()
 {
-
+    std::cout<<convertToTitle(52)<<std::endl;
 
     system("pause");
     return 0;
