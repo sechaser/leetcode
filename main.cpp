aa7c0c039978ee43f45a3080a85313c0a93bdfb5
sechaser <413406236@qq.com>
2017-04-22 15:11:50 +0800

    189.Rotate Array

--- main.cpp
+++ main.cpp
@@ -5,17 +5,45 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+//void rotate(std::vector<int>& nums, int k)
+//{
+//    if(k == 0)
+//        return;
+
+//    int sz = nums.size();
+//    k = k % sz;
+//    std::vector<int> res;
+//    for(int i = sz-k; i < sz; ++ i)
+//        res.push_back(nums[i]);
+
+//    for(int i = 0; i < sz-k; ++ i)
+//        res.push_back(nums[i]);
+
+//    nums = res;
+//}
+
+void rotate(std::vector<int>& nums, int k)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    if(!k)
+        return;
+
+    int sz = nums.size();
+    k = k % sz;
+
+    std::vector<int> temp(nums.end() - k, nums.end());
+    nums.erase(nums.end()- k, nums.end());
+    nums.insert(nums.begin(), temp.begin(), temp.end());
+}
 
 
 int main()
 {
+    std::vector<int> nums{1, 2, 3};
+    rotate(nums, 1);
 
+    for(int i = 0 ; i < nums.size(); ++ i)
+        std::cout<<nums[i]<<" ";
+    std::cout<<std::endl;
 
     system("pause");
     return 0;
