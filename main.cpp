24233c002bfad30669cb5f10c34dc44f8b69479d
sechaser <413406236@qq.com>
2017-04-22 14:29:46 +0800

    162.Find Peak Element

--- main.cpp
+++ main.cpp
@@ -5,17 +5,26 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+int findPeakElement(std::vector<int>& nums)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    if(nums.size() == 1 || nums[0] > nums[1])
+        return 0;
 
+    int sz = nums.size();
+    if(nums[sz-1] > nums[sz-2])
+        return sz-1;
+
+    for(int i = 1; i < sz-1; ++ i)
+    {
+        if((nums[i] > nums[i-1]) && (nums[i] > nums[i+1]))
+            return i;
+    }
+}
 
 int main()
 {
-
+    std::vector<int> nums{1,2,4,3};
+    std::cout<<findPeakElement(nums)<<std::endl;
 
     system("pause");
     return 0;
