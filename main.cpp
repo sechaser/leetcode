0636d492c0fe5fae9cf4a525d27f9bbe192ba7fa
sechaser <413406236@qq.com>
2017-04-22 16:03:33 +0800

    219.Contains Duplicate II

--- main.cpp
+++ main.cpp
@@ -4,17 +4,28 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <unordered_map>
 
-struct ListNode
+bool containsNearbyDuplicates(std::vector<int>& nums, int k)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    std::unordered_map<int, int> mp;
+    for(int i = 0; i < nums.size(); ++ i)
+    {
+        if(mp.count(nums[i]))
+        {
+            if(i - mp[nums[i]] <= k)
+                return true;
+        }
+        mp[nums[i]] = i;
+    }
 
+    return false;
+}
 
 int main()
 {
+    std::vector<int> nums{1, 2, 1};
+    std::cout<<containsNearbyDuplicates(nums, 3)<<std::endl;
 
 
     system("pause");
