5e142b31c27d6e33210578739f53d05f4f1b12d5
sechaser <413406236@qq.com>
2017-04-22 17:17:26 +0800

    268.Missing Number

--- main.cpp
+++ main.cpp
@@ -5,17 +5,38 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+//int missingNumber(std::vector<int>& nums)
+//{
+//    int sum1 = 0;
+//    int sz = nums.size();
+//    for(int i = 1; i <= sz; ++ i)
+//        sum1 += i;
 
+//    int sum2 = 0;
+//    for(int i = 0; i < sz; ++ i)
+//        sum2 += nums[i];
+
+//    return sum1-sum2;
+//}
+
+
+int missingNumber(std::vector<int>& nums)
+{
+    int sz = nums.size();
+    int res = 0;
+    for(int i = 0; i <= sz; ++ i)
+        res ^= i;
+
+    for(int i = 0; i < sz; ++ i)
+        res ^= nums[i];
+
+    return res;
+}
 
 int main()
 {
-
+    std::vector<int> nums{0,1,3};
+    std::cout<<missingNumber(nums)<<std::endl;
 
     system("pause");
     return 0;
