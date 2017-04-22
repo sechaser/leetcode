7ea2109c1ace40f2941f0da95510180de1404848
sechaser <413406236@qq.com>
2017-04-22 16:57:10 +0800

    238.Product of Array Except Self

--- main.cpp
+++ main.cpp
@@ -5,17 +5,33 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+std::vector<int> productExceptSelf(std::vector<int>& nums)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    int sz = nums.size();
+    std::vector<int> beginProduct(sz, 0);
+    beginProduct[0] = 1;
+    std::vector<int> afterProduct(sz, 0);
+    afterProduct[sz-1] = 1;
 
+    for(int i = 1; i < sz; ++ i)
+        beginProduct[i] = beginProduct[i-1] * nums[i-1];
+    for(int i = sz-2; i >= 0; -- i)
+        afterProduct[i] = afterProduct[i+1] * nums[i+1];
+
+    std::vector<int> res(sz, 0);
+    for(int i = 0; i < sz; ++ i)
+        res[i] = beginProduct[i] * afterProduct[i];
+
+    return res;
+}
 
 int main()
 {
+    std::vector<int> nums{1,2,3,4};
+    std::vector<int> res = productExceptSelf(nums);
 
+    for(auto &e: res)
+        std::cout<<e<<std::endl;
 
     system("pause");
     return 0;
