62cc5be0cb9cd4a8e3142a14bb3c0cdfceb26cdf
sechaser <413406236@qq.com>
2017-04-22 14:56:15 +0800

    169.Majority Element

--- main.cpp
+++ main.cpp
@@ -4,12 +4,35 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <unordered_map>
+#include <iterator>
 
+int majorityElement(std::vector<int>& nums)
+{
+    std::unordered_map<int, int> mp;
+
+    for(int i = 0; i < nums.size(); ++ i)
+        ++ mp[nums[i]];
+
+    typedef std::unordered_map<int,int>::iterator iter;
+    int accurTimes = -1, majority;
+    for(iter it = mp.begin(); it != mp.end(); ++ it)
+    {
+        if(accurTimes < it->second)
+        {
+            accurTimes = it->second;
+            majority = it->first;
+        }
+    }
+
+    return majority;
+}
 
 
 int main()
 {
-
+    std::vector<int> nums{1, 1, 2, 3};
+    std::cout<<majorityElement(nums)<<std::endl;
 
     system("pause");
     return 0;
