f024f510988545a2acca43d70e8f920841fa39af
sechaser <413406236@qq.com>
2017-04-22 16:16:55 +0800

    228.Summary Ranges

--- main.cpp
+++ main.cpp
@@ -5,17 +5,49 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+std::vector<std::string> summaryRanges(std::vector<int>& nums)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    std::vector<std::string> res;
+    if(nums.empty())
+        return res;
+
+    int start = 0;
+    for(int i = 1; i < nums.size(); ++ i)
+    {
+        if((nums[i] - nums[i-1]) != 1)
+        {
+            std::string s;
+            if(i - start > 1)
+                s = std::to_string(nums[start]) + "->" + std::to_string(nums[i-1]);
+            else
+                s = std::to_string(nums[start]);
+            res.push_back(s);
+            start = i;
+        }
+    }
+
+    if(start == nums.size() - 1)
+    {
+        std::string s = std::to_string(nums[start]);
+        res.push_back(s);
+    }
+    else
+    {
+        std::string s = std::to_string(nums[start]) + "->" + std::to_string(nums[nums.size()-1]);
+        res.push_back(s);
+    }
+
+    return res;
+}
 
 
 int main()
 {
+    std::vector<int> nums{0,2, 4,8};
+    std::vector<std::string> res = summaryRanges(nums);
 
+    for(int i = 0; i < res.size(); ++ i)
+        std::cout<<res[i]<<std::endl;
 
     system("pause");
     return 0;
