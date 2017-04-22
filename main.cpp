2d88decb6992355bbd37ec7e43ec5698cd6e98e0
sechaser <413406236@qq.com>
2017-04-22 14:46:23 +0800

    167.Majority Element

--- main.cpp
+++ main.cpp
@@ -5,17 +5,32 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+std::vector<int> twoSum(std::vector<int>& numbers, int target)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    int left = 0, right = numbers.size() - 1;
+    std::vector<int> res;
 
+    while(left < right)
+    {
+        if(numbers[left] + numbers[right] == target)
+        {
+            res.push_back(left+1);
+            res.push_back(right+1);
+            return res;
+        }
+        else if(numbers[left] + numbers[right] > target)
+            -- right;
+        else
+            ++ left;
+    }
+}
 
 int main()
 {
+    std::vector<int> numbers = {2, 7, 11, 15};
+    std::vector<int> res = twoSum(numbers, 13);
 
+    std::cout<<res[0]<<" "<<res[1]<<std::endl;
 
     system("pause");
     return 0;
