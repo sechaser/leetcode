7cb8b37d4d09f2bbce1664fecec87a48233f6f76
sechaser <413406236@qq.com>
2017-04-22 15:32:23 +0800

    209.Combination Sum III

--- main.cpp
+++ main.cpp
@@ -5,17 +5,28 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+int minSubArrayLen(int s, std::vector<int>& nums)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    int sz = nums.size(), start = 0, sum = 0, minLen = INT_MAX;
 
+    for(int i = 0; i < sz; ++ i)
+    {
+        sum += nums[i];
+        while(sum >= s)
+        {
+            minLen = std::min(minLen, i-start+1);
+            sum -= nums[start];
+            ++ start;
+        }
+    }
+
+    return minLen == INT_MAX ? 0 : minLen;
+}
 
 int main()
 {
-
+    std::vector<int> nums{2,3,1,2,4,3};
+    std::cout<<minSubArrayLen(7, nums)<<std::endl;
 
     system("pause");
     return 0;
