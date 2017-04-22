3d62c7aa98c87ae7ddafd7cb56f9567c65e49efe
sechaser <413406236@qq.com>
2017-04-22 17:28:49 +0800

    283.Move Zeroes

--- main.cpp
+++ main.cpp
@@ -5,17 +5,32 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
+void moveZeroes(std::vector<int>& nums)
 {
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+    int j = 0;
+    int sz = nums.size();
 
+    for(int i = 0; i < sz; ++ i)
+    {
+        if(nums[i] != 0)
+        {
+            nums[j] = nums[i];
+            ++ j;
+        }
+    }
+
+    for(int i = j; i < sz; ++ i)
+        nums[i] = 0;
+
+    for(int i = 0; i < sz; ++ i)
+        std::cout<<nums[i]<<" ";
+    std::cout<<std::endl;
+}
 
 int main()
 {
-
+    std::vector<int> nums{0,1,0,3,12};
+    moveZeroes(nums);
 
     system("pause");
     return 0;
