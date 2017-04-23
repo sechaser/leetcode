bd71bcd7da984d8288f08622bfc1c99e4eee4704
sechaser <413406236@qq.com>
2017-04-23 14:26:46 +0800

    258.Add Digits

--- main.cpp
+++ main.cpp
@@ -5,11 +5,28 @@
 #include <algorithm>
 #include <iomanip>
 
+int addDigits(int num)
+{
+    if(num < 10)
+        return num;
+
+    while(num >= 10)
+    {
+        int res = 0;
+        std::string temp = std::to_string(num);
+        for(int i = 0; i < temp.size(); ++ i)
+            res += (temp[i] - '0');
+
+        num = res;
+    }
+
+    return num;
+}
 
 
 int main()
 {
-
+    std::cout<<addDigits(111)<<std::endl;
 
     system("pause");
     return 0;
