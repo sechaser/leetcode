1e65b7b124c40eb9453c03762a9392366e6ce9d7
sechaser <413406236@qq.com>
2017-04-23 15:42:08 +0800

    279.Super Ugly Number

--- main.cpp
+++ main.cpp
@@ -5,11 +5,26 @@
 #include <algorithm>
 #include <iomanip>
 
+int numSquares(int n)
+{
+    if(n <= 0)
+        return 0;
 
+    std::vector<int> dp(n+1, INT_MAX);
+    dp[0] = 0;
+
+    for(int i = 1; i <= n; ++ i)
+    {
+        for(int j = 1; j*j <= i; ++ j)
+            dp[i] = std::min(dp[i], dp[i-j*j]+1);
+    }
+
+    return dp[n];
+}
 
 int main()
 {
-
+    std::cout<<numSquares(12)<<std::endl;
 
     system("pause");
     return 0;
