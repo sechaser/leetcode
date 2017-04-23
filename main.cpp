3763663871176b170dfe00d5cb44285f2b010b7e
sechaser <413406236@qq.com>
2017-04-23 10:49:40 +0800

    202.Happy Number

--- main.cpp
+++ main.cpp
@@ -5,11 +5,34 @@
 #include <algorithm>
 #include <iomanip>
 
+int solver(int n)
+{
+    int sum = 0;
+    while(n)
+    {
+        sum += (n % 10) * (n % 10);
+        n /= 10;
+    }
 
+    return sum;
+}
+
+bool isHappy(int n )
+{
+    int slow = n, fast = n;
+    do
+    {
+        slow = solver(slow);
+        fast = solver(fast);
+        fast = solver(fast);
+    }while(slow != fast);
+
+    return slow==1;
+}
 
 int main()
 {
-
+    std::cout<<isHappy(19)<<std::endl;
 
     system("pause");
     return 0;
