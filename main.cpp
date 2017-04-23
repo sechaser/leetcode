17822a8d90f03c2b9a4307898a3d9949708f3fcd
sechaser <413406236@qq.com>
2017-04-23 14:16:18 +0800

    231.Power of Two

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@
 #include <algorithm>
 #include <iomanip>
 
+bool isPowerOfTwo(int n)
+{
+    if(n <= 0)
+        return false;
+
+    bool ans = n & (n-1);
+    return !ans;
+}
 
 
 int main()
 {
-
+    std::cout<<isPowerOfTwo(5)<<std::endl;
 
     system("pause");
     return 0;
