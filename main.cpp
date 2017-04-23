54d5850a8b874f5b157c0cdebba86f907b69298e
sechaser <413406236@qq.com>
2017-04-23 08:35:42 +0800

    287.Find the Duplicate Number

--- main.cpp
+++ main.cpp
@@ -5,17 +5,46 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+//int findDuplicate(std::vector<int>& nums)
+//{
+//    for(int i = 0; i < nums.size(); ++ i)
+//    {
+//        int index = std::abs(nums[i]);
+
+//        if(nums[index - 1] < 0)
+//            return index;
+//        else
+//            nums[index-1] = -nums[index-1];
+//    }
+//}
+
+//int findDuplicate(std::vector<int>& nums)
+//{
+//    int slow = nums[0];
+//    int fast = nums[nums[0]];
+
+//    while(slow != fast)
+//    {
+//        slow = nums[slow];
+//        fast = nums[nums[fast]];
+//    }
+
+//    fast = 0;
+//    while(slow != fast)
+//    {
+//        fast = nums[fast];
+//        slow = nums[slow];
+//    }
+
+//    return slow;
+//}
+
 
 
 int main()
 {
-
+    std::vector<int> nums{1,2,3,3};
+    std::cout<<findDuplicate(nums)<<std::endl;
 
     system("pause");
     return 0;
