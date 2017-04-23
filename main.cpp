db8e202c31ede2f498f1c7ed87f4a705ec7361a1
sechaser <413406236@qq.com>
2017-04-23 14:04:25 +0800

    204.Count Primes

--- main.cpp
+++ main.cpp
@@ -5,11 +5,36 @@
 #include <algorithm>
 #include <iomanip>
 
+bool isPrime(int n)
+{
+    for(int i = 2; i <= std::sqrt(n); ++ i)
+    {
+        if(n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int countPrimes(int n)
+{
+    if(n <= 2)
+        return 0;
+
+    int count = 0;
+    for(int i = 2; i < n; ++ i)
+    {
+        if(isPrime(i))
+            ++ count;
+    }
+
+    return count;
+}
 
 
 int main()
 {
-
+    std::cout<<countPrimes(6)<<std::endl;
 
     system("pause");
     return 0;
