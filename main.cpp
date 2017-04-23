7b801d370c6dbdeb01dad9b84ae8ba668035daf0
sechaser <413406236@qq.com>
2017-04-23 14:33:34 +0800

    263.Ugly Number

--- main.cpp
+++ main.cpp
@@ -5,11 +5,26 @@
 #include <algorithm>
 #include <iomanip>
 
+bool isUgly(int num)
+{
+    if(num == 0)
+        return 0;
 
+    while(num % 2 == 0)
+        num /= 2;
+
+    while(num % 3 == 0)
+        num /= 3;
+
+    while(num % 5 == 0)
+        num /= 5;
+
+    return num == 1 ? 1 : 0;
+}
 
 int main()
 {
-
+    std::cout<<isUgly(14)<<std::endl;
 
     system("pause");
     return 0;
