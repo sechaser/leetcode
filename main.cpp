e03e58838929f08f1ed4c5bdea619daaf47b3a08
sechaser <413406236@qq.com>
2017-04-23 14:59:56 +0800

    264.Ugly Number II

--- main.cpp
+++ main.cpp
@@ -5,11 +5,36 @@
 #include <algorithm>
 #include <iomanip>
 
+int nthUglyNumber(int n)
+{
+    if(n <= 0)
+        return 0;
 
+    if(n == 1)
+        return 1;
+
+    std::vector<int> res(n, 0);
+    res[0] = 1;
+    int k2 = 0, k3 = 0, k5 = 0;
+
+    for(int i = 1; i < n; ++ i)
+    {
+        res[i] = std::min(res[k2]*2, std::min(res[k3]*3, res[k5]*5));
+
+        if(res[i] == res[k2] * 2)
+            ++ k2;
+        if(res[i] == res[k3] * 3)
+            ++ k3;
+        if(res[i] == res[k5] * 5)
+            ++ k5;
+    }
+
+    return res[n-1];
+}
 
 int main()
 {
-
+    std::cout<<nthUglyNumber(3)<<std::endl;
 
     system("pause");
     return 0;
