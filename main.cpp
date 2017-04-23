36ee9a772d1c94f60f31c39b7d0b72d6e8ed2c12
sechaser <413406236@qq.com>
2017-04-23 10:19:27 +0800

    171.Excel Sheet Column Number

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@
 #include <algorithm>
 #include <iomanip>
 
+int titleToNumber(std::string s)
+{
+    int res = 0;
+    for(int i = 0; i < s.size(); ++ i)
+        res = res * 26 + (s[i] - 'A' + 1);
+
+    return res;
+}
 
 
 int main()
 {
-
+    std::cout<<titleToNumber("AAA")<<std::endl;
 
     system("pause");
     return 0;
