287a03364b4152f552b906e776e583cd999c3c5b
sechaser <413406236@qq.com>
2017-09-10 10:37:12 +0800

    Add a solution using stack

--- main.cpp
+++ main.cpp
@@ -5,43 +5,83 @@
 #include <algorithm>
 #include <iomanip>
 
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x), next(NULL) {}
-};
+
+//void reverseWords(std::string& s)
+//{
+//    if(s.empty())
+//        return;
+
+//    std::string res;
+//    int i = s.size() - 1;
+
+//    while(i >= 0)
+//    {
+//        while(i >= 0 && s[i] == ' ')
+//            -- i;
+
+//        if(i < 0)
+//            break;
+
+//        if(!res.empty())
+//            res.push_back(' ');
+
+//        std::string t;
+//        while(i >= 0 && s[i] != ' ')
+//        {
+//            t.push_back(s[i]);
+//            -- i;
+//        }
+//        std::reverse(t.begin(), t.end());
+//        res.append(t);
+//    }
+
+//    s = res;
+//}
+
+
 
 void reverseWords(std::string& s)
 {
     if(s.empty())
         return;
 
-    std::string res;
-    int i = s.size() - 1;
+    std::stack<std::string> stk;
+    int i = 0;
 
-    while(i >= 0)
+    while(i < s.size())
     {
-        while(i >= 0 && s[i] == ' ')
-            -- i;
+        while(i < s.size() && s[i] == ' ')
+            ++ i;
 
-        if(i < 0)
+        if(i == s.size())
             break;
 
-        if(!res.empty())
-            res.push_back(' ');
-
-        std::string t;
-        while(i >= 0 && s[i] != ' ')
+        std::string temp;
+        while(i < s.size() && s[i] != ' ')
         {
-            t.push_back(s[i]);
-            -- i;
+            temp.push_back(s[i]);
+            ++ i;
         }
-        std::reverse(t.begin(), t.end());
-        res.append(t);
+
+        if(!temp.empty())
+            stk.push(temp);
     }
 
-    s = res;
+    std::string result;
+    if(stk.empty())
+    {
+        s = result;
+        return;
+    }
+
+    while(!stk.empty())
+    {
+        result = result + " " + stk.top();
+        stk.pop();
+    }
+
+    result.erase(result.begin());
+    s = result;
 }
 
 int main()
