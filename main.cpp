cf93955a7b5b7ba84027ca99ed1f93cf505dab6b
sechaser <413406236@qq.com>
2017-09-13 20:13:43 +0800

    Add one solution

--- main.cpp
+++ main.cpp
@@ -5,24 +5,45 @@
 #include <algorithm>
 #include <iomanip>
 
-std::vector<int> plusOne(std::vector<int>& digits)
-{
-    std::vector<int>::size_type sz = digits.size();
+//std::vector<int> plusOne(std::vector<int>& digits)
+//{
+//    std::vector<int>::size_type sz = digits.size();
+
+//    int carry = 0;
+//    for(int i = sz - 1; i >= 0; -- i)
+//    {
+//        if(i == sz - 1)
+//        {
+//            carry = (digits[i] + 1) / 10;
+//            digits[i] = (digits[i] + 1) % 10;
+//        }
+//        else
+//        {
+//            int tmp = carry;
+//            carry = (digits[i] + carry) / 10;
+//            digits[i] = (digits[i] + tmp) % 10;
+//        }
+//    }
+
+//    if(carry)
+//        digits.insert(digits.begin(), carry);
+
+//    return digits;
+//}
+
+
+std::vector<int> plusOne(std::vector<int>& digits) {
+    if(digits.empty())
+        return digits;
+
+    int sz = digits.size();
+    ++ digits[sz - 1];
 
     int carry = 0;
-    for(int i = sz - 1; i >= 0; -- i)
-    {
-        if(i == sz - 1)
-        {
-            carry = (digits[i] + 1) / 10;
-            digits[i] = (digits[i] + 1) % 10;
-        }
-        else
-        {
-            int tmp = carry;
-            carry = (digits[i] + carry) / 10;
-            digits[i] = (digits[i] + tmp) % 10;
-        }
+    for(int i = sz - 1; i >= 0; -- i){
+        carry += digits[i];
+        digits[i] = carry % 10;
+        carry /= 10;
     }
 
     if(carry)
@@ -30,7 +51,6 @@ std::vector<int> plusOne(std::vector<int>& digits)
 
     return digits;
 }
-
 
 int main()
 {
